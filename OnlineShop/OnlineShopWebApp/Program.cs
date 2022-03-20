9f580bd43df8e78a0c0d50d4e884bf77ecc800da
Natalia Iakovleva <tourmalina@inbox.ru>
2022-03-20 13:34:48 -0700

    Удалила комментарий

--- OnlineShop/OnlineShopWebApp/Program.cs
+++ OnlineShop/OnlineShopWebApp/Program.cs
@@ -14,7 +14,7 @@ namespace OnlineShopWebApp
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-            // test
+           
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
