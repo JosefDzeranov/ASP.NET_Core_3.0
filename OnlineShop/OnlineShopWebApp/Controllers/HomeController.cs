25377169ea22c52131fcc4174f53bfc6217afffd
Расул <rastir@yandex.ru>
2022-03-20 09:28:22 +0300

    Revert "Добавил проект."
    This reverts commit 5a07394c8a4560f3207c9be95b6d9b00e36ce5ca.

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using OnlineShopWebApp.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineShopWebApp.Models;
 
 namespace OnlineShopWebApp.Controllers
 {
