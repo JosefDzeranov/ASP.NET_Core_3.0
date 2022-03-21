3e6accf90da2de641d54cf3baa13ba408320d31d
adjutantee <romashilov3233@gmail.com>
2022-03-21 15:02:39 +0300

    Удалил комент

--- OnlineShop/WebOnlineShop/Program.cs
+++ OnlineShop/WebOnlineShop/Program.cs
@@ -13,7 +13,7 @@ namespace WebOnlineShop
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run(); // test
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
