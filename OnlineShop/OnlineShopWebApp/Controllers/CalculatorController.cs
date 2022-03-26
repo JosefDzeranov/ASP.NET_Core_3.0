67e4b5de227b6c748f7500e780a0fb95e31fd81c
Лозневой Даниил <sir.daniil0202@yandex.ru>
2022-03-26 16:11:51 +0300

    Урок 2. Домашнее задание 1. Задание 2

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double a, double b)
+        {
+            return $"{a} + {b} = {a+b}";
+        }
+    }
+}
