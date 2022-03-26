8a1c7aa19319f9f7cd8539600914922faa3877db
Лозневой Даниил <sir.daniil0202@yandex.ru>
2022-03-26 18:38:55 +0300

    создал Calc

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalcController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(double a, double b, string c)
+        {
+            if (c != "+" && c != "*" && c != "-" && c != null)
+            {
+                return "Введите в конце один из знаков '-','+' или '*'";
+            }
+            if (c == "+" || c == null)
+            {
+                return $"{a} + {b} = {a + b}";
+            }
+            if (c == "-")
+            {
+                return $"{a} - {b} = {a - b}";
+            }
+            if (c == "*")
+            {
+                return $"{a} * {b} = {a * b}";
+            }
+            return "";
+        }
+    }
+}
