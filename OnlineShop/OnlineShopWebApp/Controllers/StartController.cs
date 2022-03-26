9ff47738ceb711ee5e84ceae3562f7926238c085
Лозневой Даниил <sir.daniil0202@yandex.ru>
2022-03-26 15:53:34 +0300

    Урок 2. Домашнее задание 1. Задание 1

---
+++ OnlineShop/OnlineShopWebApp/Controllers/StartController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Hello()
+        {
+            var date1 = DateTime.Now.TimeOfDay;
+            if (date1 > new TimeSpan(0, 0, 0) & date1 < new TimeSpan(6, 0, 0))
+            {
+                return "Доброй ночи";
+            }
+            if (date1 > new TimeSpan(6, 0, 0) & date1 < new TimeSpan(12, 0, 0))
+            {
+                return "Доброй утро";
+            }
+            if (date1 > new TimeSpan(12, 0, 0) & date1 < new TimeSpan(18, 0, 0))
+            {
+                return "Добрый день";
+            }
+            if (date1 > new TimeSpan(18, 0, 0) & date1 < new TimeSpan(24, 0, 0))
+            {
+                return "Добрый вечер";
+            }
+            return "";
+        }
+    }
+}
