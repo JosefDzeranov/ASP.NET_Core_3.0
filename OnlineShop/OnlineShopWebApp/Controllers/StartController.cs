d39ca84255ddfb552cf12e843ed330b5c002989c
mardulius <kotel77@inbox.ru>
2022-03-27 11:37:59 +0300

    урок 2 задание 1 часть 1

---
+++ OnlineShop/OnlineShopWebApp/Controllers/StartController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Hello()
+        {
+            int HourNow = DateTime.Now.Hour;
+
+            if(HourNow > 0 && HourNow < 6)
+            {
+                return "Доброй ночи";
+            }
+            else if(HourNow > 6 && HourNow < 12)
+            {
+                return "Доброе утро";
+            }
+            else if(HourNow > 12 &&  HourNow < 18)
+            {
+                return "Добрый день";
+            }
+            else return "Добрый вечер"; 
+
+        }
+    }
+}
