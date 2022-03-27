19b0e41d2aa5000fad8017aaa379a7d92361287d
mardulius <kotel77@inbox.ru>
2022-03-27 12:05:47 +0300

    урок 1 задание 1 часть 2 исправил

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public double Index(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
