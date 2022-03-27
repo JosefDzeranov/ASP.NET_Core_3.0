edd81b0412e2f2c306de587718e10593d36602b5
Natalia Iakovleva <tourmalina@inbox.ru>
2022-03-27 14:21:33 -0700

    Add Calc

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalcController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Index(double a, double b, char c)
+        {
+            double res = 0;
+
+            if (c != '-' && c != '+' && c != '*' && c!='/' && c != 0)
+            {
+                return StatusCode(400, "Введите корректный знак операции");
+            }
+            else if (c == '/')
+            {
+                res = a / b;
+            }
+
+            else if (c == '-')
+            {
+                res = a - b;
+
+            }
+            else if (c == '*')
+            {
+                res = a * b;
+            }
+
+            else
+            {
+                c = '+';
+                res = a + b;
+            }
+            return Ok($"{ a} { c} { b} = { res}");
+        }
+    }
+}
