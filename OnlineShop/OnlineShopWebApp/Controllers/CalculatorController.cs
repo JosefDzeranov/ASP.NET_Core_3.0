867b00a8e43a1f3a030b8915260531c84c19e909
firokikidreamtek <firokikidreamtek@yandex.ru>
2022-03-27 23:08:11 +0300

    Done!

--- OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -33,14 +33,14 @@ namespace OnlineShopWebApp.Controllers
         }
 
             public IActionResult Privacy()
-            {
-                return View();
-            }
+        {
+            return View();
+        }
 
-            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-            public IActionResult Error()
-            {
-                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-            }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+}
