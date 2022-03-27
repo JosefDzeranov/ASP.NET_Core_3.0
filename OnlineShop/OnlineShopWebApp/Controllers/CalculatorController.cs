12ef0d4153cb6a603dc399718d79784a4bcbc2bd
Anton Kuznetsov <kuzant@gmail.com>
2022-03-27 04:03:28 +0300

    Решение задачи lesson2_1_3 c защитой от дурака.

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double a, double b, string c)
+        {
+            if(c != "+" && c != "-" && c != "*" && c != null)
+            {
+                return "Неверный оператор.\nДоступные операторы:\n< + > - для сложения\n" +
+                       "< - > - для вычитания\n< * > - для умножения\n" +
+                       "Пример правильного запроса: ../calculator/index/2/3/*\n" +
+                       "Результат: 2 * 3 = 6";
+            }
+            if(c == "-")
+            {
+                return $"{a} - {b} = {a - b}";
+            }
+            if(c == "*")
+            {
+                return $"{a} * {b} = {a * b}";
+            }
+            if(c == null)
+            {
+                return $"{a} + {b} = {a + b}";
+            }
+            return $"{a} + {b} = {a + b}";
+        }
+    }
+}
