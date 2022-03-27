d7b52339af6d5e3e9b0860d61f0a83461a1e2401
Artem Gilmanov <artemapochta@gmail.com>
2022-03-27 20:51:03 +0200

    1. Added new controller Calculate and method Index that passes two variables "a" and "b". 2. Changed the adress in Routing, method Configure, class Startup, added two variables "a" and "b".

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(int a, int b)
+        {
+            return Convert.ToString(a+b) ?? "0";
+        }
+    }
+}
