a9914f73040af182716c6872a36286ff71ba2a67
Natalia Iakovleva <tourmalina@inbox.ru>
2022-03-26 19:43:46 -0700

    Add StartController

---
+++ OnlineShop/OnlineShopWebApp/Controllers/StartController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class StartController : Controller
+    {
+        public IActionResult Hello()
+        {
+            var date = DateTime.Now;
+            var res = string.Empty;
+
+            if (date.Hour < 6)
+                res = "Доброй ночи";
+            else if (date.Hour >= 6 && date.Hour < 12)
+                res = "Доброе утро";
+            else if (date.Hour >= 12 && date.Hour < 18)
+                res = "Добрый день";
+            else
+                res = "Добрый вечер";
+
+            return Ok(res);
+        }
+    }
+}
