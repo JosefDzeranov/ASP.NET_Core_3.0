6cc9b521b111a063ccf2e773b18bbe122ec71469
mardulius <kotel77@inbox.ru>
2022-03-27 12:47:52 +0300

    урок 2 задание 1 часть 3

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double a, double b, string c)
+        {
+            if(c!= null)
+            {
+                switch (c)
+                {
+                    case "+":
+                        return $"{a} {c} {b} = {a + b}";
+
+                    case "-":
+                        return $"{a} {c} {b} = {a - b}";
+                    case "*":
+                        return $"{a} {c} {b} = {a * b}";
+                    default:
+                        return "Неверный оператор! \n" +
+                       "допускаются следующие значения:\n" +
+                       "сложение: +\n" +
+                       "вычитание: -\n" +
+                       "умножение: *";
+                }
+            }
+            return $"{a} + {b} = {a + b}";
+            
+        }
+    }
+}
