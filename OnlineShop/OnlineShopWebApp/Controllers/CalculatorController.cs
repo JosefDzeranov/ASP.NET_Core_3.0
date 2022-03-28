29854ce69cfa3cd86a62ecf98345b6a6cdbdf640
Natalia Iakovleva <tourmalina@inbox.ru>
2022-03-28 13:57:27 -0700

    Add calculator Controller

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index(double a, double b)
+        {
+
+            return Ok($"{a} + {b} = {a + b}");
+
+        }
+    }
+}
