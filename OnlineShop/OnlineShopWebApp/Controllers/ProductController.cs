feff5de457a1c9eb14f7584c1b490ad2b9beb4e2
mardulius <kotel77@inbox.ru>
2022-03-28 18:23:29 +0300

    урок 2 задание 2 часть 2

---
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+using System.Linq;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            var Products = ProdcutBase.Products;
+            var product = Products.FirstOrDefault(p=> p.Id == id);
+
+            
+            return View(product);
+        }
+    }
+}
