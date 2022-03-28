656f9b50973983566e666d1b7d765af76d450d04
Natalia Iakovleva <tourmalina@inbox.ru>
2022-03-27 21:23:47 -0700

    Add Product Info

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -18,11 +18,7 @@ namespace OnlineShopWebApp.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
+        
         public IActionResult Privacy()
         {
             return View();
@@ -33,5 +29,28 @@ namespace OnlineShopWebApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Index ()
+        {
+            var list = new List<Product>();
+
+            var product1 = new Product() { Id = 1, Name = "1", Cost = 1};
+            var product2 = new Product() { Id = 2, Name = "2", Cost = 2};
+            var product3 = new Product() { Id = 3, Name = "3", Cost = 3};
+          
+            list.Add(product1);
+            list.Add(product2);
+            list.Add(product3);
+
+            var s = string.Empty;
+
+            foreach (var item in list)
+            {
+                s += $"Id{item.Id}\nName{item.Name}\nCost{item.Cost}\n\n";
+            }
+          
+            return Ok(s);
+        }
+
     }
 }
---
+++ OnlineShop/OnlineShopWebApp/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Cost { get; set; }  
+        
+        public string Description { get; set; }
+
+        //public Product(int id, string name, decimal cost, string description)
+        //{
+        //    Id = id;
+        //    Name = name;
+        //    Cost = cost;
+        //    Description = description;
+        //}
+
+    }
+}
