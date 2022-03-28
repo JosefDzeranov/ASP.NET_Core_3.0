96330206fd2095206ee7c67a8b1776dd42bf473b
Anton Kuznetsov <kuzant@gmail.com>
2022-03-28 21:33:57 +0300

    Исправлены ошибки, учтены комментарии.

--- OnlineShop/OnlineShopWebApp/Controllers/CalcController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/CalcController.cs
@@ -8,32 +8,53 @@ namespace OnlineShopWebApp.Controllers
 {
     public class CalcController : Controller
     {
-        public string Index(double a, double b, string c)
+        public string Index(string a, string b, string c)
         {
+            double d, e;
+            if (a != null)
+            {
+                d = double.Parse(a.Replace(".", ","));
+            }
+            else
+            {
+                d = 0;
+            }
+
+            if (b != null)
+            {
+                e = double.Parse(b.Replace(".", ","));
+            }
+            else
+            {
+                e = 0;
+            }
+
             if (c != "+" && c != "-" && c != "*" && c != "/" && c != null)
             {
                 return "Неверный оператор.\nДоступные операторы:\n< + > - для сложения\n" +
-                       "< - > - для вычитания\n< * > - для умножения\n< / > - для деления\n" +
-                       "Пример правильного запроса: ../calculator/index?a=2&b=3/&c=*\n" +
+                       "< - > - для вычитания\n< * > - для умножения\n" +
+                       "Пример правильного запроса: ../calc/index?a=2&b=3&c=*" +
                        "Результат: 2 * 3 = 6";
             }
-            if (c == "-")
+
+            switch (c)
             {
-                return $"{a} - {b} = {a - b}";
+                case "-":
+                    return $"{d} - {e} = {d - e}";
+                case "*":
+                    return $"{d} * {e} = {Math.Round(d * e, 2)}";
+                case "/":
+                    if (e == 0)
+                    {
+                        return "На ноль делить нельзя.";
+                    }
+                    else
+                    {
+                        return $"{d} / {e} = {Math.Round(d / e, 2)}";
+                    }
+                default:
+                    return $"{d} + {e} = {d + e}";
             }
-            if (c == "*")
-            {
-                return $"{a} * {b} = {a * b}";
-            }
-            if(c == "/")
-            {
-                return $"{a} / {b} = {a / b}";
-            }
-            if (c == null)
-            {
-                return $"{a} + {b} = {a + b}";
-            }
-            return $"{a} + {b} = {a + b}";
         }
     }
 }
