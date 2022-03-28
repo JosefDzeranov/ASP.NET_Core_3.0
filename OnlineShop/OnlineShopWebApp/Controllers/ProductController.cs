f965fcf50cafc7e8ff422459acbe992438fc2689
Лозневой Даниил <sir.daniil0202@yandex.ru>
2022-03-28 19:50:34 +0300

    Cоздал класс Product в отдельном файле

---
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public string Index()
+        {
+            return "Id1\n100 рублей(Обслуживание)\nДебетовая карта №1\n\n" +
+                "Id2\n150 рублей(Обслуживание)\nДебетовая карта №2\n\n" +
+                "Id3\n200 рублей(Обслуживание)\nКредитная карта №1\n\n";
+        }
+    }
+}
