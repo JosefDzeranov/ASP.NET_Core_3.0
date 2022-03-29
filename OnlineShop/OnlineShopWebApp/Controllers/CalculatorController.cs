6802800d2ab9b5868076b1ef1a79ae83827569c9
Расул <rastir@yandex.ru>
2022-03-29 21:07:54 +0300

    rysaev_lesson2_1_2

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index(double a, double b)
+        {
+            return Ok($"{a} + {b} = {a + b}");
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
