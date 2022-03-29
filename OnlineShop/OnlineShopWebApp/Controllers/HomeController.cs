fc7c513a73f321882065b662416df9e8fe615b7b
Расул <rastir@yandex.ru>
2022-03-29 19:27:53 +0300

    Перенес задание rysaev_lesson2_1_1 из проекта OnlineLawServiceApp в проект OnlineShopWebApp

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -9,6 +9,27 @@ using OnlineShopWebApp.Models;
 
 namespace OnlineShopWebApp.Controllers
 {
+    public class StartController : Controller
+    {
+        public string Hello()
+        {
+            if (DateTime.Now.Hour >= 0 && DateTime.Now.Hour < 6)
+                return "Доброе утро";
+            if (DateTime.Now.Hour >= 6 && DateTime.Now.Hour < 12)
+                return "Добрый день";
+            if (DateTime.Now.Hour >= 12 && DateTime.Now.Hour < 18)
+                return "Добрый вечер";
+            if (DateTime.Now.Hour >= 18 && DateTime.Now.Hour < 24)
+                return "Доброй ночи";
+            else
+                return "";
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
