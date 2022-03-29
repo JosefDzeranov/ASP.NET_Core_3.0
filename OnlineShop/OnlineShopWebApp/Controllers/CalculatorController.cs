8ebed9b9d227e853fd6336df7515a9c484b2189c
Artem Gilmanov <artemapochta@gmail.com>
2022-03-29 08:15:00 +0200

    Started the 3rd task, but still getting the error: HTTP Error 404.11 Not Found: double escape sequence. I googled and read about the error, but still do not get where it comes from.

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(int  a, int b, char c)
+        {
+            return Convert.ToString(Action(a, b, c))??"Вы неправильно передали операцию. Обратитесь в службу поддержки.";
+        }
+
+        Func<int, int, int> summ = (a, b) => { return a + b; };
+        Func<int, int, int> differece = (a, b) => { return a - b; };
+        Func<int, int, int> multiply = (a, b) => { return a * b; };
+
+        public int Action(int a, int b, char c)
+        {
+            var result = 0;
+
+            switch (c)
+            {
+                case '+':
+                    result = summ(a,b);
+                    break;
+                case '-':
+                    result = differece(a,b);
+                    break;
+                case '*':
+                    result = multiply(a,b);
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
