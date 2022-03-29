fb52c36a7103d22eb8fb7d580d43f6d3c977e1ee
Лозневой Даниил <sir.daniil0202@yandex.ru>
2022-03-29 20:14:36 +0300

    /

--- OnlineShop/OnlineShopWebApp/Models/ProductList.cs
+++ OnlineShop/OnlineShopWebApp/Models/ProductList.cs
@@ -7,5 +7,10 @@ namespace OnlineShopWebApp.Models
 {
     public class ProductList
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+        public string Description { get; set; }
+
     }
 }
