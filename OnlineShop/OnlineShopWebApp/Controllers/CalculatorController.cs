37380efa08db87f3454956eab690bd2fd46c64df
Artem Gilmanov <artemapochta@gmail.com>
2022-03-29 08:29:45 +0200

    Changed inpts for Index funktion, from int to double.

--- OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -5,28 +5,28 @@ namespace OnlineShopWebApp.Controllers
 {
     public class CalculatorController : Controller
     {
-        public string Index(int  a, int b, char c)
+        public string Index(double  a, double b, string c)
         {
             return Convert.ToString(Action(a, b, c))??"Вы неправильно передали операцию. Обратитесь в службу поддержки.";
         }
 
-        Func<int, int, int> summ = (a, b) => { return a + b; };
-        Func<int, int, int> differece = (a, b) => { return a - b; };
-        Func<int, int, int> multiply = (a, b) => { return a * b; };
+        Func<double, double, double> summ = (a, b) => { return a + b; };
+        Func<double, double, double> differece = (a, b) => { return a - b; };
+        Func<double, double, double> multiply = (a, b) => { return a * b; };
 
-        public int Action(int a, int b, char c)
+        public double Action(double a, double b, string c)
         {
-            var result = 0;
+            var result = 0.0;
 
             switch (c)
             {
-                case '+':
+                case "+":
                     result = summ(a,b);
                     break;
-                case '-':
+                case "-":
                     result = differece(a,b);
                     break;
-                case '*':
+                case "*":
                     result = multiply(a,b);
                     break;
             }
