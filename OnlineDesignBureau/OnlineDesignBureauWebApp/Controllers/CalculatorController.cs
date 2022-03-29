1ead5a1d4fe0f656a891c35944cecf94df5db79b
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-03-29 21:54:11 +0300

    Убрал лишние методы и if_else.

--- OnlineDesignBureau/OnlineDesignBureauWebApp/Controllers/CalculatorController.cs
+++ OnlineDesignBureau/OnlineDesignBureauWebApp/Controllers/CalculatorController.cs
@@ -10,23 +10,8 @@ namespace OnlineDesignBureauWebApp.Controllers
         // Задача lession2_1_2
         public string index (double a, double b) 
         {
-            string c;
-            if (a%1 == 0 && b % 1 == 0)
-            {
-                c = Convert.ToString((int)a + (int)b);
-            }
-            else
-            {
-                c = Convert.ToString(a + b);
-            }
-            return c;
-
+            return Convert.ToString(a + b);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
