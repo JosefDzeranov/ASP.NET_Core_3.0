eb017dd0f2955d3b01c5d22cf0d24b8575b03807
Natalia Iakovleva <tourmalina@inbox.ru>
2022-03-28 22:23:31 -0700

    Add product search by id

---
+++ OnlineShop/OnlineShopWebApp/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Cost { get; set; }  
+        
+        public string Description { get; set; }
+    }
+}
