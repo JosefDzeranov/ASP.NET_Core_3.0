108f82dd17707d721ed16225070bbe2ec7ef1756
Лозневой Даниил <sir.daniil0202@yandex.ru>
2022-03-30 21:47:20 +0300

    Исправил

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -12,25 +12,13 @@ namespace OnlineShopWebApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        List<ProductList> productLists;
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            productLists = new List<ProductList>
-             {
-                new ProductList { Id = 1, Name= "Дебетовая карта №1", Cost=100, Description = "Молодежная карта. Для тех, кому от 14 лет до 21 года" },
-                new ProductList { Id = 2, Name= "Дебетовая карта №2", Cost=150, Description = "Золотая карта. Бесплатное обслуживание с подпиской, также по подписке процент на остаток" },
-                new ProductList { Id = 3, Name= "Кредитная карта №1", Cost=200, Description = "Процетная ставка голодых 20% на покупки, платежи, снятие наличных и переводы" },
-            };
         }
         public string Index()
         {
-            var s = string.Empty;
-            foreach (var product in productLists)
-            {
-                s += $"{product.Id}\n{product.Cost}\n{product.Name}\n\n";
-            }
-            return s;
+            return ProductList.Print();
         }
 
         public IActionResult Privacy()
---
+++ OnlineShop/OnlineShopWebApp/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+        public string Description { get; set; }
+        public Product(int id, string name, int cost, string description)
+        {
+            Id = id;
+            Name = name;
+            Cost = cost;
+            Description = description;
+        }
+    }
+}
