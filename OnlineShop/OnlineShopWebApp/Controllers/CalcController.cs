ee845f90de26fb21408dec316aba4bbf22438b1b
Artem Gilmanov <artemapochta@gmail.com>
2022-03-31 10:10:18 +0200

    Task 2_1_4 done.

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalcController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(double a, double b, string c)
+        {
+            switch (c)
+            {
+                case null:
+                case "+": return $"{a}+{b}={a + b}";
+                case "-": return $"{a}-{b}={a - b}";
+                case "*": return $"{a}-{b}={a - b}";
+                case "/": return b != 0 ? $"{a}/{b}={a / b}":"Делить на ноль нельзя.";
+                             // if true ? result 1          : otherwise result 2;
+
+                default:
+
+                    return "Вы неправильно передали операцию. Можно передавать только +, -, *.";
+            }
+        }
+    }
+}
