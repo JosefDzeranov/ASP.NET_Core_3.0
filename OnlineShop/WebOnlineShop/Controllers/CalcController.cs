7601b6e2367f42bbc43b0aab729ce58e128d911c
adjutantee <romashilov3233@gmail.com>
2022-03-31 14:46:57 +0300

    Izagakhmaev lesson 2_1_4

---
+++ OnlineShop/WebOnlineShop/Controllers/CalcController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebOnlineShop.Models;
+using System.Diagnostics;
+
+namespace WebOnlineShop.Controllers
+{
+    public class Calc : Controller
+    {
+        public string Index(double a, double b, string c)
+        {
+            if(c == "+")
+            {
+                return $"{a}+{b} = {a + b}";
+            }
+            else if(c == "-")
+            {
+                return $"{a}-{b} = {a - b}";
+            }
+            else if (c == "*")
+            {
+                return $"{a}/{b} = {a / b}";
+            }
+
+            return $"{a}/{b} = {a / b}";
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
