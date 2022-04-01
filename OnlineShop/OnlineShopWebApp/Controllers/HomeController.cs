74ae4527d8b59b78999f55b5ec42f6bfbd168c32
Artem Gilmanov <artemapochta@gmail.com>
2022-04-01 08:31:27 +0200

    Following the video, task 2_2_1 done. 1. Added class Product 2. Added class ProductRepository

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -11,114 +11,26 @@ namespace OnlineShopWebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ProductReposititory productReposititory;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            productReposititory = new ProductReposititory();
         }
 
-        public List<string> Index()
+        public string Index()
         {
-            var items = new List<Item>();
-            var item1 = new Item();
-            var item2 = new Item();
-            var item3 = new Item();
-
-            items.Add(item1);
-            items.Add(item2);
-            items.Add(item3);
-
-            //foreach (var item in items)
-            //{
-            //    Console.WriteLine(item.Id);
-            //    Console.WriteLine(item.Name);
-            //    Console.WriteLine(item.Cost);
-            //    Console.WriteLine();
-            //}
-
-            var itemsInfo = new List<string>();
-
-            foreach (var item in items)
+            var products = productReposititory.GetAll();
+            var result = String.Empty;
+            foreach (var product in products)
             {
-                var itemInfo = item.ToString().Split(',');
-
-                for (int i = 0; i < itemInfo.Length; i++)
-                {
-                    itemsInfo.Add(itemInfo[i]);
-                }
+                result += product + "\n\n";
             }
 
-            return itemsInfo;
+            return result;
         }
 
-        public class Item
-        {
-            string id;
-            string name;
-            string cost;
-            string description;
-            private static int cnt;
-
-            public string Id
-            {
-                get
-                {
-                    return id;
-                }
-                set
-                {
-                    id = $"Id{cnt}";
-                }
-
-            }
-            public string Name
-            {
-                get
-                {
-                    return name;
-                }
-                set
-                {
-                    name = $"Name{cnt}";
-                }
-            }
-            public string Cost
-            {
-                get
-                {
-                    return cost;
-                }
-                set
-                {
-                    cost = $"Cost{cnt}";
-                }
-            }
-            public string Description
-            {
-                get
-                {
-                    return description;
-                }
-                set
-                {
-                    description = $"{value}{cnt}";
-                }
-            }
-
-            public Item()
-            {
-                cnt++;
-                Id = id;
-                Name = name;
-                Cost = cost;
-            }
-
-            public override string ToString()
-            {
-                return $"{Id},{Name},{Cost}";
-            }
-        }
+       
 
         public IActionResult Privacy()
         {
---
+++ OnlineShop/OnlineShopWebApp/ProductReposititory.cs
@@ -0,0 +1,24 @@
+﻿using OnlineShopWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp
+{
+    public class ProductReposititory
+    {
+        private static List<Product> products = new List<Product>()
+        {
+            new Product("Name1", 10, "Disc1"),
+            new Product("Name2", 20, "Disc2"),
+            new Product("Name3", 30, "Disc3"),
+            new Product("Name4", 40, "Disc4"),
+        };
+
+        public List<Product> GetAll()
+        {
+            return products;
+        }
+    }
+}
