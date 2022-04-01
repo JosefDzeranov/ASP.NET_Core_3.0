bc4665157d2b2309e5eea9dd2377e913e4720663
Natalia Iakovleva <tourmalina@inbox.ru>
2022-04-01 00:35:03 -0700

    Add ProductDataSource

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using OnlineShopWebApp.DataSources;
 using OnlineShopWebApp.Models;
 using System;
 using System.Collections.Generic;
@@ -32,25 +33,12 @@ namespace OnlineShopWebApp.Controllers
 
         public IActionResult Index ()
         {
-            var list = new List<Product>();
+            var productSource = new ProductDataSource();
+            var products = productSource.GetAllProducts();
 
-            var product1 = new Product() { Id = 1, Name = "Тур в Турцию", Cost = 50000};
-            var product2 = new Product() { Id = 2, Name = "Тур в Грецию", Cost = 60000};
-            var product3 = new Product() { Id = 3, Name = "Тур в Болгарию", Cost = 45000};
-          
-            list.Add(product1);
-            list.Add(product2);
-            list.Add(product3);
-
-            var s = string.Empty;
-
-            foreach (var item in list)
-            {
-                s += $"Id {item.Id}\nName {item.Name}\nCost {item.Cost}\n\n";
-            }
-          
-            return Ok(s);
+            var result = string.Join("\r\n", products);
+         
+            return Ok(result);
         }
-
     }
 }
--- OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using OnlineShopWebApp.DataSources;
 using OnlineShopWebApp.Models;
 using System;
 using System.Collections.Generic;
@@ -14,22 +15,14 @@ namespace OnlineShopWebApp.Controllers
       
         public IActionResult Index(int id)
         {
-            var list = new List<Product>();
+            var productSource = new ProductDataSource();
 
-            var product1 = new Product() { Id = 1, Name = "1", Cost = 1, Description = "1" };
-            var product2 = new Product() { Id = 2, Name = "2", Cost = 2, Description = "2" };
-            var product3 = new Product() { Id = 3, Name = "3", Cost = 3, Description = "3" };
-
-            list.Add(product1);
-            list.Add(product2);
-            list.Add(product3);
-
-            var s = list.FirstOrDefault(x => x.Id == id);
+            var s = productSource.GetProductById(id)?.ToString();
 
             if (s!=null)
                 return Ok(s);
 
-            return BadRequest("Продукт не найден");
+            return BadRequest($"Продукт c ID № {id} не существует");
         }
     }
 }
