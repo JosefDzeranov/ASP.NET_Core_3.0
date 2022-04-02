f0b350768d050e70113e316cb92d46e9e67ebab6
Расул <rastir@yandex.ru>
2022-04-02 12:12:57 +0300

    Edited description products. Corrected index method in ProductController.

--- OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
@@ -9,7 +9,7 @@ namespace OnlineShopWebApp.Controllers
         {
             var requestedProduct = StorageProducts.TryGetProduct(id);
             if ( requestedProduct != null)
-            {
+            { 
                 if (requestedProduct.Id.Equals(1) || requestedProduct.Id.Equals(2) || requestedProduct.Id.Equals(3) || requestedProduct.Id.Equals(4) || requestedProduct.Id.Equals(5))
                     return $"{requestedProduct.Id}\r\n{requestedProduct.Name}\r\n{requestedProduct.Cost}\r\n{requestedProduct.Description}";
                 else
