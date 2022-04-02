3d0d14c24915a3d5a5baeef52dd097ff855da23a
Boris B <bvboris87@outlook.com>
2022-04-02 13:45:09 +0300

    removed unused code

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -1,28 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using OnlineShopWebApp.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace OnlineShopWebApp.Controllers
 {
     public class HomeController : Controller
     {
-       
+
         public string Index()
         {
             return ProductManager.ShowProducts();
         }
 
-      
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using OnlineShopWebApp.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace OnlineShopWebApp.Controllers
 {
     public class ProductController : Controller
     {
-       
+
 
         public string Index(int id)
         {
             return ProductManager.FindProduct(id);
         }
-              
 
-      
+
+
     }
 }
