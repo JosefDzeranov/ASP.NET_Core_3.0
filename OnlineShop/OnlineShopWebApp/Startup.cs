102c9b302e9b165fca7b80d242766c76fbf9863c
Artem Gilmanov <artemapochta@gmail.com>
2022-04-03 18:02:40 +0200

    1. Added card for each product; 2. Added myStyle in .css file;

--- OnlineShop/OnlineShopWebApp/Startup.cs
+++ OnlineShop/OnlineShopWebApp/Startup.cs
@@ -4,10 +4,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace OnlineShopWebApp
 {
@@ -29,16 +25,7 @@ namespace OnlineShopWebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
