c4bfd8e18aa7638869196edbe7e9c5cb28f7c5cd
Расул <rastir@yandex.ru>
2022-04-03 18:37:17 +0300

    Correct return message Product/Index

--- OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
@@ -11,18 +11,15 @@ namespace OnlineShopWebApp.Controllers
         {
             storageProducts = new StorageProducts();
         }
+
         public string Index(int id)
         {
             var requestedProduct = StorageProducts.TryGetProduct(id);
-            if (requestedProduct != null)
-            {
-                if (requestedProduct.Id.Equals(1) || requestedProduct.Id.Equals(2) || requestedProduct.Id.Equals(3) || requestedProduct.Id.Equals(4) || requestedProduct.Id.Equals(5))
-                    return $"{requestedProduct.Id}\r\n{requestedProduct.Name}\r\n{requestedProduct.Cost}\r\n{requestedProduct.Description}";
-                else
-                    return "Такой услуги нет в списке, введите корректное значение 1-5";
-            }
+
+            if (requestedProduct != null && ((requestedProduct.Id.Equals(1) || requestedProduct.Id.Equals(2) || requestedProduct.Id.Equals(3) || requestedProduct.Id.Equals(4) || requestedProduct.Id.Equals(5))))
+                return $"{requestedProduct.Id}\r\n{requestedProduct.Name}\r\n{requestedProduct.Cost}\r\n{requestedProduct.Description}";
             else
-                return $"Услуги с id={id} не имеется!";//--StorageProducts.ShowProducts();
+                return "Такой услуги нет в списке, введите корректное значение 1-5";
         }
     }
 }
