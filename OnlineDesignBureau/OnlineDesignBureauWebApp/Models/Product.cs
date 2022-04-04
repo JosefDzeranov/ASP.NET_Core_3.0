549732b04d66ee740315184ff986e22ab22a19cc
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-04-04 11:37:38 +0300

    Добавил set в Product в поля Name, Cost, Description

--- OnlineDesignBureau/OnlineDesignBureauWebApp/Models/Product.cs
+++ OnlineDesignBureau/OnlineDesignBureauWebApp/Models/Product.cs
@@ -7,9 +7,9 @@ namespace OnlineDesignBureauWebApp.Models
     public class Product
     {
         public int Id { get; }
-        public string Name { get; }
-        public decimal Cost { get; }
-        public string Description { get; }
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+        public string Description { get; set; }
         public Product(int id, string name, decimal cost, string description)
         {
             Id = id;
