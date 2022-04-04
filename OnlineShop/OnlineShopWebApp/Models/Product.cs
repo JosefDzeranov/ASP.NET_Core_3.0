1c991c85f491d5b2b58a42255308829b4f8c7592
mardulius <kotel77@inbox.ru>
2022-04-04 20:29:38 +0300

    add description to product in productBase

--- OnlineShop/OnlineShopWebApp/Models/Product.cs
+++ OnlineShop/OnlineShopWebApp/Models/Product.cs
@@ -8,11 +8,12 @@
         public decimal Cost { get; set; }
         public string Description { get; set; }
         public string ImgPath { get; set; }
-        public Product(int id, string name, decimal cost, string imgPath)
+        public Product(int id, string name, decimal cost, string description, string imgPath)
         {
             this.Id = id;
             this.Name = name;
             this.Cost = cost;
+            this.Description = description;
             this.ImgPath = imgPath;
            
         }
--- OnlineShop/OnlineShopWebApp/ProdcutBase.cs
+++ OnlineShop/OnlineShopWebApp/ProdcutBase.cs
@@ -7,11 +7,11 @@ namespace OnlineShopWebApp.Models
     {
         public static List<Product> products = new List<Product>()
         {
-           new Product(1, "Незнайка на луне", 345.6m , "/images/book.png" ),
-           new Product(2, "Что делать?", 556.5m, "/images/book.png"),
-           new Product(3, "Остров сокровищ", 999.0m, "/images/book.png"),
-           new Product(4, "Одисея капитана Блада", 1360.4m, "/images/book.png"),
-           new Product(5, "Война и мир", 790.6m, "/images/book.png")
+           new Product(1, "Незнайка на луне", 345.6m , "Описание товара", "/images/book.png" ),
+           new Product(2, "Что делать?", 556.5m,"Описание товара", "/images/book.png"),
+           new Product(3, "Остров сокровищ", 999.0m,"Описание товара", "/images/book.png"),
+           new Product(4, "Одисея капитана Блада", 1360.4m,"Описание товара", "/images/book.png"),
+           new Product(5, "Война и мир", 790.6m,"Описание товара", "/images/book.png")
 
         };
 
