5de1d92efb407dd3851e6213a4d4b6e3e8648538
adjutantee <romashilov3233@gmail.com>
2022-04-04 15:52:42 +0300

    add the models

---
+++ OnlineShop/WebOnlineShop/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WebOnlineShop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+        public string Description { get; set; }
+
+    }
+}
