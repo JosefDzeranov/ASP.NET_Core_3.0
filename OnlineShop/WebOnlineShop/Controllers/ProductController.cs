a4cae33000d35895a4dfb493c370cc8c5fdc5890
adjutantee <romashilov3233@gmail.com>
2022-04-04 17:30:21 +0300

    ud

---
+++ OnlineShop/WebOnlineShop/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebOnlineShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductRepository productRepository;
+
+        public ProductController()
+        {
+            productRepository = new ProductRepository();
+        }
+        public string Index(int id)
+        {
+            var product = productRepository.GetById(id);
+        }
+    }
+}
