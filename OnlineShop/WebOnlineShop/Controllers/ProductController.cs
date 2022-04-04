53c982db61e6e5833f1882b78f579e22dd136a47
adjutantee <romashilov3233@gmail.com>
2022-04-04 17:00:58 +0300

    add logic

--- OnlineShop/WebOnlineShop/Controllers/ProductController.cs
+++ OnlineShop/WebOnlineShop/Controllers/ProductController.cs
@@ -6,16 +6,15 @@ namespace WebOnlineShop.Controllers
 {
     public class ProductController : Controller
     {
-        private static List<Product> products = new List<Product>()
+        private readonly ProductRepository productRepository;
+        public ProductController()
         {
-            new Product("Iphone 13 PRO", 80000, "Desc1"),
-            new Product("Sony PS 5", 90000, "Desc2"),
-            new Product("RTX 3080", 400000, "Desc3"),
-            new Product("HyperX Keybard", 7600, "Desc4"),
-        };
+            productRepository = new ProductRepository();
+        }
 
         public string Index()
         {
+            var products = productRepository.GetProducts();
             var result = "";
             foreach (var product in products)
             {
---
+++ OnlineShop/WebOnlineShop/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using WebOnlineShop.Models;
+
+namespace WebOnlineShop
+{
+    public class ProductRepository
+    {
+        private static List<Product> products = new List<Product>()
+        {
+            new Product("Iphone 13 PRO", 80000, "Desc1"),
+            new Product("Sony PS 5", 90000, "Desc2"),
+            new Product("RTX 3080", 400000, "Desc3"),
+            new Product("HyperX Keybard", 7600, "Desc4"),
+        };
+
+        public List<Product> GetProducts()
+        {
+            return products;
+        }
+    }
+}
