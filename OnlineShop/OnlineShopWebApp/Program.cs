996c702bd7fd8c8c8eb7956a7b6e80cc51413a00
Boris B <bvboris87@outlook.com>
2022-04-05 21:58:26 +0300

    Произошел конфликт веток. В итоге задание 3.1 "потерялось". Задание 3.1 было выполнено в ветке bilonenko_lesson3_1, которую я слил в данную ветку (т.е. сейчас 2 ветки с заданием 3.2)

--- OnlineShop/OnlineShopWebApp/Program.cs
+++ OnlineShop/OnlineShopWebApp/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Logging;
 
 namespace OnlineShopWebApp
 {
+    //Произошел конфликт веток.В итоге задание 3.1 "потерялось". Задание 3.1 было выполнено в ветке bilonenko_lesson3_1, которую я слил в данную ветку(т.е.сейчас 2 ветки с заданием 3.2)
     public class Program
     {
         public static void Main(string[] args)
