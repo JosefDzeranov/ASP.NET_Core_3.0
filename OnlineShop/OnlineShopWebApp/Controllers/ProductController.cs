5a63ea4268dd94915901bb6862f8a0913b24a4d9
Расул <rastir@yandex.ru>
2022-04-05 16:47:56 +0300

    removed "&& requestedProduct.Id.Equals(id)"

--- OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
@@ -8,7 +8,7 @@ namespace OnlineShopWebApp.Controllers
         public string Index(int id)
         {
             var requestedProduct = StorageProducts.TryGetProduct(id);
-            if (requestedProduct != null && requestedProduct.Id.Equals(id))
+            if (requestedProduct != null)
                 return $"{requestedProduct.Id}\r\n{requestedProduct.Name}\r\n{requestedProduct.Cost}\r\n{requestedProduct.Description}";
             else
                 return "Такой услуги нет в списке, введите корректное значение 1-5";
