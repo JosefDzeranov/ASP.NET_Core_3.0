88d9a1ca0ac82610990aeb206812410b05b6c97b
Anton <kuzant@gmail.com>
2022-04-05 15:02:50 +0300

    Created BasketController and Basket Page, added Basket Button to Header.

---
+++ OnlineShop/OnlineShopWebApp/Controllers/BasketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class BasketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
