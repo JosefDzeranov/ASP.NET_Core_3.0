e10bb8f5ef9d0ab8f27806e507699926a588dab6
mardulius <kotel77@inbox.ru>
2022-04-05 19:37:22 +0300

    add cart class

---
+++ OnlineShop/OnlineShopWebApp/Models/Cart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace OnlineShopWebApp.Models
+{
+    public class Cart
+    {
+        public List<Product> Products { get; set; }
+
+        public Cart()
+        {
+            Products = new List<Product>();
+        }
+        public void AddToCart(Product product)
+        {
+            Products.Add(product);
+        }
+
+        public List<Product> TryGetAll()
+        {
+            return Products ?? null;
+        }
+
+        public void RemoveFromCart(Product product)
+        {
+            Products.Remove(product);
+        }
+    }
+}
