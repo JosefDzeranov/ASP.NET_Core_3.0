d717dad84bc55e9272c4dc9a5d27196c1471385e
mardulius <kotel77@inbox.ru>
2022-04-05 19:23:51 +0300

    add  cartController and add cart ref to button in view

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
