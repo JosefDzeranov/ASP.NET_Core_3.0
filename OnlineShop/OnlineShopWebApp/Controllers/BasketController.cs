b5de243d29300f585753ec9f370985b1e4a83622
Artem Gilmanov <artemapochta@gmail.com>
2022-04-06 08:09:55 +0200

    1. Removed shadow from navbar. (Don't need any shadows for my layout) 2.Changed type of navbar container to container-fluid. 3. Added View with basket. 4. Added new Controller BasketController 5. Added class OrderRepository, inharited from ProductrRepository. 6. Added class Order, inharited from Product

---
+++ OnlineShop/OnlineShopWebApp/Controllers/BasketController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OnlineShopWebApp.Models;
+
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly OrderReposititory orderReposititory;
+
+        public BasketController()
+        {
+            orderReposititory = new OrderReposititory();
+        }
+
+        public IActionResult Order(int id)
+        {
+            var order = orderReposititory.TryGetById(id);
+            return View(order);
+
+        }
+
+    }
+}
