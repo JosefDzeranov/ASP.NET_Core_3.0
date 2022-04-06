9c50db17b2b1fd1283a03f09edd31c1b660354d5
Лозневой Даниил <sir.daniil0202@yandex.ru>
2022-04-06 23:11:25 +0300

    Пытался добавить корзину

---
+++ OnlineShop/OnlineShopWebApp/Models/Basket.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Models
+{    public class Basket
+    {
+        public Product Product { get; }
+        public int Quantity { get; }
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/Models/BasketStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Models
+{
+    public class BasketStorage
+    {
+        private static List<Product> basket = new List<Product>();
+        public static List<Product> AddToBasket(int id)
+        {
+            basket.Add(ProductsStorage.TryGetProduct(id));
+            return basket;
+        }
+    }
+}
