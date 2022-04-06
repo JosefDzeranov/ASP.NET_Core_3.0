e76a871d60fecf83b2d2f1522156ec18ba7e1495
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-04-06 08:41:59 +0300

    добавил в Product дополнительные поля

--- OnlineDesignBureau/OnlineDesignBureauWebApp/Models/Product.cs
+++ OnlineDesignBureau/OnlineDesignBureauWebApp/Models/Product.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System;
 
 namespace OnlineDesignBureauWebApp.Models
 {
@@ -7,17 +7,24 @@ namespace OnlineDesignBureauWebApp.Models
         public int Id { get; }
         public string Name { get; set; }
         public decimal Cost { get; set; }
+        public double Square { get; set; }
+        public double Width { get; set; }
+        public double Length { get; set; }
         public string Description { get; set; }
-        public Product(int id, string name, decimal cost, string description)
+        public Product(int id, string name, decimal cost, double square, double width, double length, string description)
         {
             Id = id;
             Name = name;
             Cost = cost;
+            Square = square;
+            Width = width;
+            Length = length;
             Description = description;
         }
         public override string ToString()
         {
-            return $"{Id}\n{Name}\n{Cost}";
+            return $"Id={Id}\nName={Name}\nCost={Cost}\nSquare={Square}м2\nDimensions={Math.Round(Width, 0)}x{Math.Round(Length, 0)}\n{Description}";
         }
+    }
     }
 }
