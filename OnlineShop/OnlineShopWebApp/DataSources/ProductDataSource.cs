732834680a1d8e0edeefef280758b1868ba8273d
IakovlevaNatalia <69373349+IakovlevaNatalia@users.noreply.github.com>
2022-04-05 19:41:55 -0700

    Iakovleva lesson 2_2_1 (#36)
    * Add StartController
    
    * Add Calculator
    
    * Fixed Calculator
    
    * Add Product Info
    
    * remove unused controllers
    
    * remove comments
    
    * addressing comments
    
    * Iakovleva lesson2_2_2 (#50)
    
    * Add product search by id
    
    * Add product search by id
    
    * Add product search by id
    
    * Add ProductDataSource

---
+++ OnlineShop/OnlineShopWebApp/DataSources/ProductDataSource.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using OnlineShopWebApp.Models;
+using System.Linq;
+
+namespace OnlineShopWebApp.DataSources
+{
+    public class ProductDataSource
+    {
+        private static List<Product> products = new List<Product>()
+        {
+            new Product("Тур в Турцию", 50000, "Тур в Турцию за 50000 рублей"),
+            new Product("Тур в Грецию", 60000, "Тур в Грецию за 60000 рублей"),
+            new Product("Тур в Болгарию", 45000, "Тур в Болгарию за 45000 рублей")
+        };
+
+        public IEnumerable<Product> GetAllProducts ()
+        {
+            return products;
+        }
+
+        public Product GetProductById (int id)
+        {
+            return products.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
