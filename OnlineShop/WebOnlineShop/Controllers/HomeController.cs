019f994e6a3e69f8572d6035962741418e0131b6
adjutantee <romashilov3233@gmail.com>
2022-04-07 09:38:03 +0300

    Переименование контроллеров

--- OnlineShop/WebOnlineShop/Controllers/HomeController.cs
+++ OnlineShop/WebOnlineShop/Controllers/HomeController.cs
@@ -1,37 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using WebOnlineShop.Models;
 
 namespace WebOnlineShop.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ProductRepository productRepository;
+        public HomeController()
         {
-            _logger = logger;
+            productRepository = new ProductRepository();
         }
 
-        public IActionResult Index()
+        public string Index()
         {
-            return View();
+            var products = productRepository.GetProducts();
+            var result = "";
+            foreach (var product in products)
+            {
+                result += product + "\n\n";
+            }
+            return result;
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
---
+++ OnlineShop/WebOnlineShop/Controllers/HomsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebOnlineShop.Models;
+
+namespace WebOnlineShop.Controllers
+{
+    public class HomsController : Controller
+    {
+        private readonly ILogger<HomsController> _logger;
+
+        public HomsController(ILogger<HomsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
