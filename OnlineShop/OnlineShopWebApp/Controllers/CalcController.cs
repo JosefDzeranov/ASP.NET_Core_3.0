20798e5a9318758ddc77c5510048bb8edbc0d2f0
Татьяна Рыбакова <tatiana_rybakova@mail.ru>
2022-04-07 21:25:23 +0300

    rybakova_lesson2_1_4

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalcController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(double a, double b, string c)
+        {
+            switch (c)
+            {
+                case "-": return $"{a} - {b} = {a - b}";
+                case "*": return $"{a} * {b} = {a * b}";
+                case "/": return $"{a} / {b} = {a/b}";
+                case null:
+                case "+": return $"{a} + {b} = {a + b}";
+                default: return "Введите знак операции";
+            }
+
+
+
+        }
+    }
+}
