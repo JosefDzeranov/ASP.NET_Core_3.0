961986650131ad7fbb44f496e352d91093a1e466
Татьяна Рыбакова <tatiana_rybakova@mail.ru>
2022-04-07 18:39:16 +0300

    rybakova_lesson2_1_3

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double a, double b, string c)
+        {
+            switch (c)
+            {
+                case "-": return $"{a} - {b} = {a-b}"; 
+                case "*": return $"{a} * {b} = {a*b}";
+                case null:
+                case "+": return $"{a} + {b} = {a+b}";
+                default: return "Введены неверные данные";
+            }
+            
+            
+            
+        }
+    }
+}
--- OnlineShop/OnlineShopWebApp/Startup.cs
+++ OnlineShop/OnlineShopWebApp/Startup.cs
@@ -50,7 +50,7 @@ namespace OnlineShopWebApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Calculator}/{action=Index}/{a?}/{b?}/{c?}");
             });
         }
     }
