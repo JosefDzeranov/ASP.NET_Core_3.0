6a3cbf82559cf01aaacaafbcac09b342eba4a572
Татьяна Рыбакова <tatiana_rybakova@mail.ru>
2022-04-08 17:25:33 +0300

    Добавила, что на ноль делить нельзя

--- OnlineShop/OnlineShopWebApp/Controllers/CalcController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/CalcController.cs
@@ -14,7 +14,7 @@ namespace OnlineShopWebApp.Controllers
             {
                 case "-": return $"{a} - {b} = {a - b}";
                 case "*": return $"{a} * {b} = {a * b}";
-                case "/": return $"{a} / {b} = {a/b}";
+                case "/": return b!=0 ? $"{a} / {b} = {a/b}": "На ноль делить нельзя";
                 case null:
                 case "+": return $"{a} + {b} = {a + b}";
                 default: return "Введите знак операции";
