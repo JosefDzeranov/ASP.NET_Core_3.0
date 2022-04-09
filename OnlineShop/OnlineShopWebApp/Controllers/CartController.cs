d362eeac4b44e21f4cc5bb551a830e15aac7bc11
Лозневой Даниил <sir.daniil0202@yandex.ru>
2022-04-09 22:28:35 +0300

    Добавил классы Cart, CartItem, CartsStorage и View для корзины

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CartController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            var cart = CartsStorage.TryGetByUserId(Constants.UserId);
+            return View(cart);
+        }
+        public IActionResult Add(int productId)
+        {
+            var product = ProductsStorage.TryGetProduct(productId);
+            CartsStorage.Add(product, Constants.UserId);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/Models/CartsStorage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Models
+{
+    public static class CartsStorage
+    {
+        private static List<Cart> carts = new List<Cart>();
+
+        internal static Cart TryGetByUserId(string userId)
+        {
+            return carts.FirstOrDefault(x => x.UserId == userId);
+        }
+
+        internal static void Add(Product product, string userId)
+        {
+            var existingCart = TryGetByUserId(userId);
+            if (existingCart == null)
+            {
+                var newCart = new Cart
+                {
+                    Id = Guid.NewGuid(),
+                    UserId = userId,
+                    Items = new List<CartItem>
+                    {
+                        new CartItem
+                        {
+                            Id = Guid.NewGuid(),
+                            Amount = 1,
+                            Product = product
+                        }
+                    }
+                };
+                carts.Add(newCart);
+            }
+            else
+            {
+                var existingCartItem = existingCart.Items.FirstOrDefault(x => x.Product.Id == product.Id);
+                if (existingCartItem != null)
+                {
+                    existingCartItem.Amount += 1;
+                }
+                else
+                {
+                    existingCart.Items.Add(new CartItem
+                    {
+                        Id = Guid.NewGuid(),
+                        Amount = 1,
+                        Product = product
+                    });
+                }
+            }
+        }
+    }
+}
