48dff36da47b7bcaa5f046ad54fc63018ecf4d93
IakovlevaNatalia <69373349+IakovlevaNatalia@users.noreply.github.com>
2022-04-11 07:26:59 -0700

    Iakovleva lesson3_1 (#79)
    * Add StartController
    
    * Add Calculator
    
    * Fixed Calculator
    
    * Add Product Info
    
    * remove unused controllers
    
    * Add product search by id
    
    * remove comments
    
    * Add product search by id
    
    * addressing comments
    
    * Add product search by id
    
    * Add ProductDataSource
    
    * Complete lesson 3, task 1
    
    * Add partial product view
    
    * Add interfaces
    
    * delete

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -31,14 +31,12 @@ namespace OnlineShopWebApp.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public IActionResult Index ()
+        public IActionResult Index()
         {
             var productSource = new ProductDataSource();
             var products = productSource.GetAllProducts();
 
-            var result = string.Join("\r\n", products);
-         
-            return Ok(result);
+            return View(products);
         }
     }
 }
---
+++ OnlineShop/OnlineShopWebApp/Services/InMemoryProductDataSource.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using OnlineShopWebApp.Models;
+using System.Linq;
+
+namespace OnlineShopWebApp.Services
+{
+	public class InMemoryProductDataSource : IProductDataSource
+	{
+		private List<Product> products = new List<Product>()
+		{
+			new Product("Тур в Турцию", 50000, "Тур в Турцию за 50000 рублей", "/images/turkey.jpg"),
+			new Product("Тур в Грецию", 60000, "Тур в Грецию за 60000 рублей", "/images/greece.jpg"),
+			new Product("Тур в Болгарию", 45000, "Тур в Болгарию за 45000 рублей", "/images/bulgary.jpg")
+		};
+
+		public IEnumerable<Product> GetAllProducts()
+		{
+			return products;
+		}
+
+		public Product GetProductById(int id)
+		{
+			return products.FirstOrDefault(x => x.Id == id);
+		}
+	}
+}
