b83510e4ab8fc018d5f70783cd2a074f57068597
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-04-12 23:13:18 +0300

    Добавил ссылку в шапке на аккаунт

---
+++ OnlineShop/OnlineShopWebApp/Controllers/PersonController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineDesignBureauWebApp.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly ProductCatalog productCatalog;
+        public PersonController()
+        {
+            productCatalog = new ProductCatalog();
+        }
+        public IActionResult Index()
+        {
+            if (ProductCatalog.products.Count == 0)
+                productCatalog.ReadToJson("projects_for_sale");
+            return View(ProductCatalog.products);
+        }
+    }
+}
