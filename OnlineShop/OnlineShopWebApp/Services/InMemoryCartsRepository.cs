aff4876353652f9d37da152351bd9224701bae74
Natalia Iakovleva <tourmalina@inbox.ru>
2022-04-11 18:18:58 -0700

    Lesson 4_3

--- OnlineShop/OnlineShopWebApp/Services/InMemoryCartsRepository.cs
+++ OnlineShop/OnlineShopWebApp/Services/InMemoryCartsRepository.cs
@@ -4,30 +4,67 @@ using System.Linq;
 
 namespace OnlineShopWebApp.Services
 {
-	public class InMemoryCartsRepository : ICartRepository
-	{
+    public class InMemoryCartsRepository : ICartRepository
+    {
+        private readonly IProductDataSource productDataSource;
 
-		public void Add(Product product)
-		{
-			if (CartItems.ContainsKey(product))
-			{
-				CartItems[product] += 1;
-			}
-			else
-			{
-				CartItems.Add(product, 1);
-			}
+        public InMemoryCartsRepository(IProductDataSource productDataSource)
+        {
+            this.productDataSource = productDataSource;
+        }
 
-		}
-		public Dictionary<Product, int> CartItems { get; set; } = new Dictionary<Product, int>();
+        public void Add(Product product)
+        {
+            if (CartItems.ContainsKey(product))
+            {
+                CartItems[product] += 1;
+            }
+            else
+            {
+                CartItems.Add(product, 1);
+            }
 
-		public decimal Cost
-		{
-			get
-			{
-				return CartItems.Sum(item => item.Key.Cost * item.Value);
-			}
-		}
+        }
 
-	}
+
+        public void Add(int id)
+        {
+            var product = productDataSource.GetProductById(id);
+            Add(product);
+        }
+
+        public void Remove(int id)
+        {
+            var product = productDataSource.GetProductById(id);
+            Remove(product);
+        }
+
+        public void Remove(Product product)
+        {
+            if (CartItems.ContainsKey(product))
+            {
+                if (CartItems[product] > 1)
+                    CartItems[product] -= 1;
+                else
+                    CartItems.Remove(product);
+            }
+
+        }
+
+        public void RemoveAll()
+        {
+            CartItems.Clear();
+        }
+
+        public Dictionary<Product, int> CartItems { get; set; } = new Dictionary<Product, int>();
+
+        public decimal Cost
+        {
+            get
+            {
+                return CartItems.Sum(item => item.Key.Cost * item.Value);
+            }
+        }
+
+    }
 }
