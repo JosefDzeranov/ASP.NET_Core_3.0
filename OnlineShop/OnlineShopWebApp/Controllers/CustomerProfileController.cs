1da3a0120fdcb8d29a54211175c45fb2d7e98b73
Natalia Iakovleva <tourmalina@inbox.ru>
2022-04-13 14:13:55 -0700

    Lesson4_4

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CustomerProfileController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Services;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CustomerProfileController : Controller
+    {
+        private readonly ICustomerProfile customerProfile;
+
+        public CustomerProfileController(ICustomerProfile customerProfile)
+        {
+            this.customerProfile = customerProfile;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
