0bd2d419ae7061148a0c6c79207fb096d07af4c4
mardulius <kotel77@inbox.ru>
2022-04-14 19:49:06 +0300

    add orderView index

--- OnlineShop/OnlineShopWebApp/Controllers/OrderController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
 using OnlineShopWebApp.Services;
 
 namespace OnlineShopWebApp.Controllers
@@ -7,8 +8,24 @@ namespace OnlineShopWebApp.Controllers
     {
         private readonly ICartBase cartBase;
         private readonly IOrderBase orderBase;
+        public OrderController(ICartBase cartBase, IOrderBase orderBase)
+        {
+            this.cartBase = cartBase;
+            this.orderBase = orderBase;
+        }
+
         public IActionResult Index()
         {
+            var existingCart = cartBase.TryGetByUserId(Const.UserId);
+
+            return View(existingCart);
+        }
+
+        public IActionResult Create()
+        {
+            var existingCart = cartBase.TryGetByUserId(Const.UserId);
+            orderBase.Add(existingCart);
+            cartBase.RemoveAll(Const.UserId);
             return View();
         }
     }
