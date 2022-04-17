5a066838b92137e2ebb27c584d0d75317696f6e9
Boris B <bvboris87@gmail.com>
2022-04-17 15:14:41 +0300

    Registration page is ready

---
+++ OnlineShop/OnlineShopWebApp/Controllers/RegistrationController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class RegistrationController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string login, string password, string confirmedpassword)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+       
+    }
+
+}
