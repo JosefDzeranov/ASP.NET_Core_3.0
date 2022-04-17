9c37266c0b4f647d41fd212612cb149ed6dbc423
Boris B <bvboris87@gmail.com>
2022-04-17 13:18:01 +0300

    saving orders to JSON file in wwwroot

--- OnlineShop/OnlineShopWebApp/Controllers/OrderController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/OrderController.cs
@@ -19,22 +19,19 @@ namespace OnlineShopWebApp.Controllers
         {
             var cart = cartManager.TryGetCartByUserID(Constants.UserId);
 
-            //orderManager.SaveOrder(new Order(cart, Constants.UserId));
-            //var order = orderManager.TryGetOrderById(Constants.UserId);
-
             return View(cart);
         }
 
         public IActionResult MakeOrder(string name, string adress, string email)
         {
             var cart = cartManager.TryGetCartByUserID(Constants.UserId);
-            
+
 
             var order = new Order(cart, Constants.UserId);
             order.Adress = adress;
             order.Name = name;
             order.Email = email;
-            
+
             orderManager.SaveOrder(order);
             cartManager.RemoveCartLines(cart);
 
