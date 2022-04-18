80d67ac83f83e029f9c42978b4641cca1bf95d2c
mardulius <kotel77@inbox.ru>
2022-04-18 14:20:23 +0300

    fix errors

--- OnlineShop/OnlineShopWebApp/Controllers/OrderController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/OrderController.cs
@@ -8,16 +8,10 @@ namespace OnlineShopWebApp.Controllers
 {
     public class OrderController : Controller
     {
-<<<<<<< HEAD
-        private readonly ICartBase cartBase;
-        private readonly IOrderBase orderBase;
-        
-        public OrderController(ICartBase cartBase, IOrderBase orderBase)
-=======
+
         private readonly ICartRepository cartRepository;
         private readonly IOrderRepository orderRepository;
         public OrderController(ICartRepository cartRepository, IOrderRepository orderRepository)
->>>>>>> karpunin_lesson4_5
         {
             this.cartRepository = cartRepository;
             this.orderRepository = orderRepository;
@@ -36,33 +30,13 @@ namespace OnlineShopWebApp.Controllers
         [HttpPost]
         public IActionResult Create(Order order)
         {
-<<<<<<< HEAD
-            var existingCart = cartBase.TryGetByUserId(Const.UserId);
-            order.Cart = existingCart;
-            orderBase.Add(order);
-            cartBase.Clear(Const.UserId);
 
-
-
-=======
             var existingCart = cartRepository.TryGetByUserId(Const.UserId);
-            var order = new Order()
-            {
-                Id = Guid.NewGuid(),
-                UserId = Const.UserId,
-                Cart = existingCart,
-                FirstName = firstname,
-                LastName = lastname,
-                Email = email,
-                Phone = phone,
-                Address = address,
-                Created = DateTime.Now,
-                TotalCost = existingCart.TotalCost,
-
-            };
+            order.Cart = existingCart;
+        
             orderRepository.Add(order);
-            cartRepository.RemoveAll(Const.UserId);
->>>>>>> karpunin_lesson4_5
+            cartRepository.Clear(Const.UserId);
+
             return View();
         }
 
