7f61ecfd3842bb39a050ed883048fb9205664f84
mardulius <kotel77@inbox.ru>
2022-04-18 20:13:46 +0300

    add AccountController and Login view

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string login, string password, bool remember)
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
