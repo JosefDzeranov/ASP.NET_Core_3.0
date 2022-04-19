b1d701f85db77eb2caddcde0906975c58e37eca7
Татьяна Рыбакова <tatiana_rybakova@mail.ru>
2022-04-20 00:50:07 +0300

    rybakova_lesson4_5

--- OnlineShop/OnlineShopWebApp/Models/Product.cs
+++ OnlineShop/OnlineShopWebApp/Models/Product.cs
@@ -12,14 +12,16 @@ namespace OnlineShopWebApp.Models
         public string Name { get; }
         public decimal Cost { get; }
         public string Description { get; }
+        public int Pages { get; }
 
 
-        public Product(string name, decimal cost, string description)
+        public Product(string name, decimal cost, string description, int pages)
         {
             Id = constantCounter;
             Name = name;
             Cost = cost;
             Description = description;
+            Pages = pages;
             constantCounter++;
         }
 
