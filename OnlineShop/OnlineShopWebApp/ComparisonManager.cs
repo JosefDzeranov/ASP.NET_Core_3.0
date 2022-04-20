d25e1cc93ecb9a89732c0f9e11877583f5e8d627
Boris B <bvboris87@outlook.com>
2022-04-20 21:52:41 +0300

    IComparison has UserId

--- OnlineShop/OnlineShopWebApp/ComparisonManager.cs
+++ OnlineShop/OnlineShopWebApp/ComparisonManager.cs
@@ -5,10 +5,12 @@ namespace OnlineShopWebApp.Models
 {
     public class ComparisonManager : IComparison
     {
-
+        public string UserId { get; set; } 
+    
         public List<Product> Products { get; } = new List<Product>();
 
-
+        
+     
         public void AddProduct(Product product)
         {
             Products.Add(product);
