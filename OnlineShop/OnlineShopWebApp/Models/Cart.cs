0522878898300cfdf7ec86dab1b445ddea9f3900
Татьяна Рыбакова <tatiana_rybakova@mail.ru>
2022-04-20 17:04:12 +0300

    rybakova_lesson5_1

--- OnlineShop/OnlineShopWebApp/Models/Cart.cs
+++ OnlineShop/OnlineShopWebApp/Models/Cart.cs
@@ -14,7 +14,15 @@ namespace OnlineShopWebApp.Models
         {
             get
             {
-                return Items.Sum(x => x.Cost);
+                return Items?.Sum(x => x.Cost) ?? 0;
+            }
+        }
+
+        public decimal Amount
+        {
+            get
+            {
+                return Items?.Sum(x => x.Amount) ?? 0;
             }
         }
     }
