1e4812d50822cb57c5c9530e5a883e6647bd8958
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-04-21 13:51:23 +0300

    Добавил отчет о совершении заказа и сохранение данных в json

--- OnlineShop/OnlineShopWebApp/Controllers/MakingPurchaseController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/MakingPurchaseController.cs
@@ -16,5 +16,12 @@ namespace OnlineDesignBureauWebApp.Controllers
         {
             return View(buyerStorage.FindBuyer(buyerId));
         }
+        public IActionResult ReportTransaction(int buyerId)
+        {
+            buyerStorage.FindBuyer(buyerId).OrdersList.AddRange(buyerStorage.FindBuyer(buyerId).CartList);
+            buyerStorage.FindBuyer(buyerId).CartList.Clear();
+            buyerStorage.WriteToStorage();
+            return View();
+        }
     }
 }
