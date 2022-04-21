fc0ec0e148a1ccd228b403db6535baaadb6d5345
Татьяна Рыбакова <tatiana_rybakova@mail.ru>
2022-04-21 20:52:46 +0300

    rybakova_lesson5_3

---
+++ OnlineShop/OnlineShopWebApp/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Authorization()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Enter(string login, string password, bool remember)
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult Registrate(string login, string password1, string password2)
+        {
+            return View();
+        }
+    }
+}
