157e31d286924b4569a8f22d6a3bff6b0b0eb407
Anton <kuzant@gmail.com>
2022-04-22 01:05:46 +0300

    Initial commit.

---
+++ OnlineShop/OnlineShopWebApp/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OnlineShopWebApp.Models
+{
+    public class User
+    {
+        public Guid Id { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
