0a683d86b573e8f49c22f6479d9d1d01197f4530
Artem Gilmanov <artemapochta@gmail.com>
2022-04-21 20:58:45 +0200

    1. Created actions for each of the button in AdminController 2. Created Views for each action 3. Moved AdminPanel to the separate View, that will be partial 4. Added partial view for each admin view 5. Linked Administrator Item in Header, to AdminPanel action and corresponding Vew

--- OnlineShop/OnlineShopWebApp/Controllers/AdminController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/AdminController.cs
@@ -8,12 +8,23 @@ namespace OnlineShopWebApp.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        public IActionResult AdminPanel()
         {
             return View();
         }
-
+        public IActionResult Orders()
+        {
+            return View();
+        }
+        public IActionResult Products()
+        {
+            return View();
+        }
         public IActionResult Status()
+        {
+            return View();
+        }
+        public IActionResult Users()
         {
             return View();
         }
