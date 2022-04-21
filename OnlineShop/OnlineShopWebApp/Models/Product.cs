22bb6b6b1330f26b32087ab84736cdc7483e494c
mardulius <kotel77@inbox.ru>
2022-04-21 21:22:37 +0300

    edit product class, add autoincrement Id

--- OnlineShop/OnlineShopWebApp/Models/Product.cs
+++ OnlineShop/OnlineShopWebApp/Models/Product.cs
@@ -2,20 +2,20 @@
 {
     public class Product
     {
-
+        private static int CurrentId = 1;
         public int Id { get;}
         public string Name { get; set; }
         public decimal Cost { get; set; }
         public string Description { get; set; }
         public string ImgPath { get; set; }
-        public Product(int id, string name, decimal cost, string description, string imgPath)
+        public Product(string name, decimal cost, string description, string imgPath)
         {
-            this.Id = id;
+            this.Id = CurrentId;
             this.Name = name;
             this.Cost = cost;
             this.Description = description;
             this.ImgPath = imgPath;
-           
+           CurrentId++;
         }
         
     }
--- OnlineShop/OnlineShopWebApp/Services/ProductRepository.cs
+++ OnlineShop/OnlineShopWebApp/Services/ProductRepository.cs
@@ -8,16 +8,15 @@ namespace OnlineShopWebApp.Services
     {
         private List<Product> products = new List<Product>()
         {
-           new Product(1, "Незнайка на луне", 345.6m , "Описание товара", "/images/book.png" ),
-           new Product(2, "Что делать?", 556.5m,"Описание товара", "/images/book.png"),
-           new Product(3, "Остров сокровищ", 999.0m,"Описание товара", "/images/book.png"),
-           new Product(4, "Одисея капитана Блада", 1360.4m,"Описание товара", "/images/book.png"),
-           new Product(5, "Война и мир", 790.6m,"Описание товара", "/images/book.png")
+           new Product("Незнайка на луне", 345.6m , "Описание товара", "/images/book.png" ),
+           new Product("Что делать?", 556.5m,"Описание товара", "/images/book.png"),
+           new Product("Остров сокровищ", 999.0m,"Описание товара", "/images/book.png"),
+           new Product("Одисея капитана Блада", 1360.4m,"Описание товара", "/images/book.png"),
+           new Product("Война и мир", 790.6m,"Описание товара", "/images/book.png")
 
         };
 
         
-
         public List<Product> GetAll()
         {
             return products;
