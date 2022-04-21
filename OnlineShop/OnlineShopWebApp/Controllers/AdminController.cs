f445b2f8e4edb141fbb68b019dd81899eee69e1a
mardulius <kotel77@inbox.ru>
2022-04-21 16:45:27 +0300

    add AdminController and view

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Services;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IOrderRepository orderRepository;
+
+        public AdminController(IOrderRepository orderRepository)
+        {
+            this.orderRepository = orderRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var existingOrders = orderRepository.TryGetAll();
+
+            return View(existingOrders);
+        }
+    }
+}
