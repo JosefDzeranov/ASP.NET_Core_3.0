a848d037ffff203997d0e1990454bbb1561812bd
Anton Kuznetsov <kuzant@gmail.com>
2022-04-22 04:11:24 +0300

    Task lesson5_2 done.

---
+++ OnlineShop/OnlineShopWebApp/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string login, string password, bool remember)
+        {
+            return View();
+        }
+    }
+}
