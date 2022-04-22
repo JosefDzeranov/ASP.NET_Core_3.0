c3a1025aaf26294cb3dcf61f47a3d7fc7d2db870
Anton Kuznetsov <kuzant@gmail.com>
2022-04-22 22:37:58 +0300

    Deleted unneccessary Index in AdminController.

--- OnlineShop/OnlineShopWebApp/Controllers/AdminController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/AdminController.cs
@@ -4,11 +4,6 @@ namespace OnlineShopWebApp.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         public IActionResult Orders()
         {
             return View();
