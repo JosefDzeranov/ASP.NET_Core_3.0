990b299fc9eeafd5ce59941f5298b8b3570a06ef
Anton Kuznetsov <kuzant@gmail.com>
2022-04-22 04:45:08 +0300

    Task lesson5_3 done.

---
+++ OnlineShop/OnlineShopWebApp/Controllers/SignupController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class SignupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Signup(string login, string password, string confirm)
+        {
+            return View();
+        }
+    }
+}
