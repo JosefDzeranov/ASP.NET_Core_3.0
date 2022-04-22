119d09415532251a0620cdbc74e6f28e6c39fd70
Natalia Iakovleva <tourmalina@inbox.ru>
2022-04-21 19:59:12 -0700

    Fixes lesson5_2
    Remove constructors, fixed files

--- OnlineShop/OnlineShopWebApp/Models/Customer.cs
+++ OnlineShop/OnlineShopWebApp/Models/Customer.cs
@@ -2,17 +2,6 @@
 {
     public class Customer
     {
-        public Customer(string name, string surname, int id, string email, string adress, string phone)
-        {
-            Name = name;
-            Surname = surname;
-            Id = id;
-            Email = email;
-            Adress = adress;
-            Phone = phone;
-
-        }
-
         public string Name { get; set; }
         public string Surname { get; set; }
         public int Id { get; set; }
--- OnlineShop/OnlineShopWebApp/Models/UserCredentials.cs
+++ OnlineShop/OnlineShopWebApp/Models/UserCredentials.cs
@@ -2,17 +2,6 @@
 {
     public class UserCredentials
     {
-        public UserCredentials()
-        {
-        }
-
-        public UserCredentials(string userName, string password, bool rememberMe)
-        {
-            UserName = userName;
-            Password = password;
-            RememberMe = rememberMe;
-        }
-
         public string UserName { get; set; }
         public string Password { get; set; }  
         public bool RememberMe { get; set; }
