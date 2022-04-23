d194578791b9ef544ad8c5acdf804163be971a08
Лозневой Даниил <sir.daniil0202@yandex.ru>
2022-04-23 23:25:36 +0300

    Добавил кнопку и панель Админа

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Orders()
+        {
+            return View();
+        }
+        public IActionResult Users()
+        {
+            return View();
+        }
+        public IActionResult Roles()
+        {
+            return View();
+        }
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}
