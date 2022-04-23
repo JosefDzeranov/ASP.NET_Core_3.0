ff7bcd8289520720e5a40cfbcd0c5c1b05967726
Лозневой Даниил <sir.daniil0202@yandex.ru>
2022-04-23 22:50:58 +0300

    Добавил форму регистрации

---
+++ OnlineShop/OnlineShopWebApp/Models/Register.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class Register
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool ConfirmPassword { get; set; }
+    }
+}
