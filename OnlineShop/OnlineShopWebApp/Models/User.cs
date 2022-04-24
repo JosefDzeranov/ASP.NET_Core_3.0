7dae0f17c66bd9ed2fb128702798678778cd2a32
Artem Gilmanov <artemapochta@gmail.com>
2022-04-24 17:08:12 +0200

    Corrected small (English) mistakes

--- OnlineShop/OnlineShopWebApp/Models/User.cs
+++ OnlineShop/OnlineShopWebApp/Models/User.cs
@@ -9,15 +9,15 @@ namespace OnlineShopWebApp.Models
     public class User
     {
         [Required(ErrorMessage = "Please enter Firstname.")]
-        [StringLength(20, MinimumLength = 2, ErrorMessage = "Firstname should has length between 2 and 10 characters.")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Firstname should have length between 2 and 20 characters.")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Please enter SecondName.")]
-        [StringLength(20, MinimumLength = 2, ErrorMessage = "SecondName should has length between 2 and 10 characters.")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "SecondName should have length between 2 and 20 characters.")]
         public string SecondName { get; set; }
 
         [Required(ErrorMessage ="Please enter Login.")]
-        [StringLength(20, MinimumLength =2, ErrorMessage ="Login should has length between 2 and 10 characters.")]
+        [StringLength(20, MinimumLength =2, ErrorMessage = "Login should have length between 2 and 20 characters.")]
         public string Login { get; set; }
 
         [Required(ErrorMessage = "Please enter E-mail.")]
@@ -25,7 +25,7 @@ namespace OnlineShopWebApp.Models
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Please enter Password.")]
-        [StringLength(15, MinimumLength = 5, ErrorMessage = "Password should has length between 5 and 15 characters.")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Password should have length between 5 and 15 characters.")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Please enter Password again.")]
