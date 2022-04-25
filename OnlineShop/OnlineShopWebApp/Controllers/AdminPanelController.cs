2d58231b46b645f6e1b459469ef110ea455aed3a
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-04-25 10:11:19 +0300

    Добавил панель администратора с боковым меню

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AdminPanelController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Interfase;
+
+namespace OOnlineShopWebApp.Controllers
+{
+
+    public class AdminPanelController : Controller
+    {
+        public IActionResult Index(int personId)
+        {
+            return View();
+        }
+        public IActionResult Orders(int personId)
+        {
+            return View();
+        }
+        public IActionResult Users(int personId)
+        {
+            return View();
+        }
+        public IActionResult Roles(int personId)
+        {
+            return View();
+        }
+        public IActionResult Products(int personId)
+        {
+            return View();
+        }
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/Views/Shared/Components/AdminPanel/AdminPanel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Interfase;
+
+namespace OnlineShopWebApp.Views.Shared.Components.AdminPanel
+{
+    public class AdminPanel : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("Menu");
+        }
+    }
+}
