a78e3b797463872f81a54f69b932d01697496224
mardulius <kotel77@inbox.ru>
2022-04-25 19:04:32 +0300

    add annotation to LoginModelView

--- OnlineShop/OnlineShopWebApp/ViewModels/LoginViewModel.cs
+++ OnlineShop/OnlineShopWebApp/ViewModels/LoginViewModel.cs
@@ -1,9 +1,14 @@
-﻿namespace OnlineShopWebApp.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.ViewModels
 {
     public class LoginViewModel
     {
+        [Required (ErrorMessage = "не указано имя")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "не указан пароль")]
         public string Password { get; set; }
+       
         public bool RememberMe { get; set; }
     }
 }
