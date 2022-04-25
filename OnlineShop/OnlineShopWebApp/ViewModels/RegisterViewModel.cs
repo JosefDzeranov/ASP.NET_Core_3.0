c62f98a12bf3bb7ed001689b0c4c62d9d7797222
mardulius <kotel77@inbox.ru>
2022-04-25 19:36:10 +0300

    adding annotatrions to RegisterViewModel

--- OnlineShop/OnlineShopWebApp/ViewModels/RegisterViewModel.cs
+++ OnlineShop/OnlineShopWebApp/ViewModels/RegisterViewModel.cs
@@ -1,10 +1,16 @@
-﻿namespace OnlineShopWebApp.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.ViewModels
 {
     public class RegisterViewModel
     {
-
+        [Required(ErrorMessage = "не указано имя")]
+        [StringLength(16, MinimumLength =2, ErrorMessage ="имя должно быть от 2 до 16 символов")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Не указан пароль")]
         public string Password { get; set; }
+        [Required(ErrorMessage = "Не указан повтор пароля")]
+        [Compare("Password", ErrorMessage ="пароли не совпадают")]
         public string ConfirmPassword{ get; set; }
     }
 }
