afb4408cc9df28fcb4cef2b9ae8f3833ad0d82ae
Boris B <bvboris87@outlook.com>
2022-04-25 22:10:17 +0300

    IProductManager.ProductList has been renamed

--- OnlineShop/OnlineShopWebApp/IProductManager.cs
+++ OnlineShop/OnlineShopWebApp/IProductManager.cs
@@ -4,8 +4,8 @@ namespace OnlineShopWebApp
 {
     public interface IProductManager
     {
-        public List<Product> productList { get; set; }
-        public List<Product> AllProducts();
+        public List<Product> ProductList { get; set; }
+     
         public Product FindProduct(int id);
 
     }
