e119998d5a62d4d9c800dd4ac58564d8467c1d95
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-04-25 07:06:56 +0300

    Создал форму авторизации

---
+++ OnlineShop/OnlineShopWebApp/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Enter(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+        [HttpPost]
+        public IActionResult NewUser(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- OnlineShop/OnlineShopWebApp/Models/User.cs
+++ OnlineShop/OnlineShopWebApp/Models/User.cs
@@ -6,11 +6,13 @@ namespace OnlineShopWebApp.Models
     public class User
     {
         public int Id { get; set; }
+        public string login { get; set; }
         public string Fistname { get; set; }
         public string Secondname { get; set; }
         public string Surname { get; set; }
         public int Age { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
 
     }
 }
