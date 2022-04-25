67384ff5aa879fba5a7dffc99265fe40993f727b
Лозневой Даниил <sir.daniil0202@yandex.ru>
2022-04-25 21:06:38 +0300

    поправил Register

--- OnlineShop/OnlineShopWebApp/Models/Register.cs
+++ OnlineShop/OnlineShopWebApp/Models/Register.cs
@@ -4,6 +4,6 @@
     {
         public string Username { get; set; }
         public string Password { get; set; }
-        public bool ConfirmPassword { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
