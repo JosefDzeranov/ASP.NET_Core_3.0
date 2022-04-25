2f7d7c566283a2ac0e22de169d2c6ae0a4f74152
mardulius <kotel77@inbox.ru>
2022-04-25 19:43:37 +0300

    adding tag-helpers to register view

--- OnlineShop/OnlineShopWebApp/ViewModels/RegisterViewModel.cs
+++ OnlineShop/OnlineShopWebApp/ViewModels/RegisterViewModel.cs
@@ -7,10 +7,18 @@ namespace OnlineShopWebApp.ViewModels
         [Required(ErrorMessage = "не указано имя")]
         [StringLength(16, MinimumLength =2, ErrorMessage ="имя должно быть от 2 до 16 символов")]
         public string Name { get; set; }
+
         [Required(ErrorMessage = "Не указан пароль")]
         public string Password { get; set; }
+
         [Required(ErrorMessage = "Не указан повтор пароля")]
         [Compare("Password", ErrorMessage ="пароли не совпадают")]
         public string ConfirmPassword{ get; set; }
+        
+        
+        
+        [Required(ErrorMessage = "не указан email")]
+        [EmailAddress(ErrorMessage ="некорректный формат email")]
+        public string Email { get; set; }
     }
 }
