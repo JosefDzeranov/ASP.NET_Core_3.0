51b5f0598da07fcd4f26c97b45b989bfd5775f95
Лозневой Даниил <sir.daniil0202@yandex.ru>
2022-04-25 19:58:15 +0300

    поправил View Login

--- OnlineShop/OnlineShopWebApp/Models/Login.cs
+++ OnlineShop/OnlineShopWebApp/Models/Login.cs
@@ -7,7 +7,7 @@ namespace OnlineShopWebApp.Models
 {
     public class Login
     {
-        public string Username { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
         public bool RememberMe { get; set; }
     }
