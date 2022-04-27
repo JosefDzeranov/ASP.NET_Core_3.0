fe488bd89dbff5a66eddadf92f30da7d95be630a
mardulius <kotel77@inbox.ru>
2022-04-27 16:03:11 +0300

    adding order validation into  index view order controller

--- OnlineShop/OnlineShopWebApp/Models/Order.cs
+++ OnlineShop/OnlineShopWebApp/Models/Order.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace OnlineShopWebApp.Models
 {
@@ -10,10 +11,18 @@ namespace OnlineShopWebApp.Models
         public DateTime Created { get; set; } = DateTime.Now;
 
         public OrderStatus Status { get; set; } = OrderStatus.New;
+        [Required (ErrorMessage = "Имя не заполнено")]
+        [StringLength(16, MinimumLength = 2, ErrorMessage = "имя должно быть от 2 до 16 символов")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Фамилия не заполнена")]
+        [StringLength(25, MinimumLength = 2, ErrorMessage = "имя должно быть от 2 до 25 символов")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "не указан email")]
+        [EmailAddress(ErrorMessage = "некорректный формат email")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "не указан телефон")]
         public string Phone { get; set; }
+        [Required(ErrorMessage = "не указан адрес")]
         public string Address { get; set; }
         public decimal TotalCost { get; set; }
 
