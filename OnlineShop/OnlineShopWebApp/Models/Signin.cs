a3412475e95812dfc162c6c24957d4b7d6d9d1f4
Anton <kuzant@gmail.com>
2022-04-27 12:19:20 +0300

    Initial commit

--- OnlineShop/OnlineShopWebApp/Models/Signin.cs
+++ OnlineShop/OnlineShopWebApp/Models/Signin.cs
@@ -1,8 +1,14 @@
-﻿namespace OnlineShopWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
 {
     public class SignIn
     {
+        [Required(ErrorMessage = "Email is not specified.")]
+        [EmailAddress(ErrorMessage = "Enter a valid email.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Enter a passwodrd.")]
         public string Password { get; set; }
         public bool RememberMe { get; set; }
     }
