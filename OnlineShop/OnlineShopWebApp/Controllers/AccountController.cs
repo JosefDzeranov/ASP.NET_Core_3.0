bd1402d7cb4e20d7ad105f8c454d321c7c0e1f24
firokikidreamtek <firokikidreamtek@yandex.ru>
2022-04-27 09:46:01 +0300

    Added an authorization page with saving the data entered in the fields

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Authorization(Authorization authorization)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/Models/Authorization.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Models
+{
+    public class Authorization
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
