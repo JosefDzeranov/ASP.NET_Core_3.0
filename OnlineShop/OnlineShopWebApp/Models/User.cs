55abf1ac2bfe01163b258e2b0000db212cd1ba70
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-04-27 09:10:37 +0300

    Выполнил валидацию

--- OnlineShop/OnlineShopWebApp/Models/User.cs
+++ OnlineShop/OnlineShopWebApp/Models/User.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using OnlineShopWebApp.Models;
 
 namespace OnlineShopWebApp.Models
 {
     public class User
     {
+        
         public int Id { get; set; }
-        public string login { get; set; }
+
+        [Required(ErrorMessage = "Не указан логин")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 50 символов")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Не указано имя")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 50 символов")]
         public string Fistname { get; set; }
+
+        [Required(ErrorMessage = "Не указано отчество")]
         public string Secondname { get; set; }
+
+        [Required(ErrorMessage = "Не указана фамилия")]
         public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Не указан возраст")]
+        [Range(16, 110, ErrorMessage = "Возраст должен быть от 16 до 110")]
         public int Age { get; set; }
+
+        [Required(ErrorMessage = "Не указан email")]
+        [EmailAddress(ErrorMessage = "Некорректный адрес")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Не указан повторный пароль")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string PasswordConfirm { get; set; }
 
     }
 }
