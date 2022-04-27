ec1f7d04c59551aeca81d7c072c96dcfceccf693
Anton Kuznetsov <kuzant@gmail.com>
2022-04-27 16:13:15 +0300

    Validation on client done.

--- OnlineShop/OnlineShopWebApp/Models/Signin.cs
+++ OnlineShop/OnlineShopWebApp/Models/Signin.cs
@@ -8,7 +8,7 @@ namespace OnlineShopWebApp.Models
         [EmailAddress(ErrorMessage = "Enter a valid email.")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Enter a passwodrd.")]
+        [Required(ErrorMessage = "Enter a password.")]
         public string Password { get; set; }
         public bool RememberMe { get; set; }
     }
--- OnlineShop/OnlineShopWebApp/Models/Signup.cs
+++ OnlineShop/OnlineShopWebApp/Models/Signup.cs
@@ -1,11 +1,26 @@
-﻿namespace OnlineShopWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
 {
     public class SignUp
     {
+        [Required(ErrorMessage = "Please, enter your Firstname.")]
+        [StringLength(25, MinimumLength = 2, ErrorMessage = "The name shouldn't be less than 2 and more than 25 characters.")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please, enter your Lastname.")]
+        [StringLength(25, MinimumLength = 2, ErrorMessage = "The name shouldn't be less than 2 and more than 25 characters.")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is not specified.")]
+        [EmailAddress(ErrorMessage = "Enter a valid email.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Enter a password.")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm a password.")]
+        [Compare("Password", ErrorMessage = "Рasswords don't match.")]
         public string ConfirmPassword { get; set; }
     }
 }
