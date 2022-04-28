37ea0e7070abe2ea57416adde1df8cf42b93aacb
IakovlevaNatalia <69373349+IakovlevaNatalia@users.noreply.github.com>
2022-04-28 08:47:29 -0700

    Iakovleva_lesson6_2 (#241)
    * Lesson 5_0
    
    * Lesson5_1
    
    * Lesson5_2
    
    * fixes
    
    * Lesson5_3
    
    * work in progress
    
    * c
    
    * Lesson5-4. Add new admin views.
    
    * Lesson 6-1
    
    * Lesson6-2

--- OnlineShop/OnlineShopWebApp/Models/Customer.cs
+++ OnlineShop/OnlineShopWebApp/Models/Customer.cs
@@ -1,23 +1,25 @@
-﻿namespace OnlineShopWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace OnlineShopWebApp.Models
 {
     public class Customer
     {
-        public Customer(string name, string surname, int id, string email, string adress, string phone)
-        {
-            Name = name;
-            Surname = surname;
-            Id = id;
-            Email = email;
-            Adress = adress;
-            Phone = phone;
-
-        }
-
+        [Required(ErrorMessage ="Не указано имя")]
+        [StringLength(25, MinimumLength =2, ErrorMessage ="Имя должно быть от 2 от 25 симоволов")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Не указана фамилия")]
+        [StringLength(25, MinimumLength = 2, ErrorMessage = "Фамилия должна быть от 2 от 25 симоволов")]
         public string Surname { get; set; }
         public int Id { get; set; }
+        
+        [Required(ErrorMessage ="Не указан email")]
+        [EmailAddress(ErrorMessage = "Введите корректный e-mail")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Не указан адрес")]
         public string Adress { get; set; }
+
+        [Required(ErrorMessage = "Не указан телефон")]
         public string Phone { get; set; }
     }
 }
