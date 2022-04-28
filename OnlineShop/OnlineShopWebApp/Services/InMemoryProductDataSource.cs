57be611ed57ae6b92b6bca554e3cf195b233c209
Natalia Iakovleva <tourmalina@inbox.ru>
2022-04-28 11:29:51 -0700

    remove unused

--- OnlineShop/OnlineShopWebApp/Services/InMemoryProductDataSource.cs
+++ OnlineShop/OnlineShopWebApp/Services/InMemoryProductDataSource.cs
@@ -42,7 +42,6 @@ namespace OnlineShopWebApp.Services
 			existing.Name = product.Name;
 			existing.Cost = product.Cost;
 			existing.Description = product.Description;
-			//existing.ImagePath = product.ImagePath;
         }
 	}
 }
