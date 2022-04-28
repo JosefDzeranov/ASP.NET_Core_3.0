23cb5297e862d5d9d66e1fa90ee3eb77bd499453
firokikidreamtek <firokikidreamtek@yandex.ru>
2022-04-28 09:13:14 +0300

    Added button "Админ панель" in header, AdminController and pages for they

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Orders()
+        {
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            return View();
+        }
+
+        public IActionResult Roles()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}
