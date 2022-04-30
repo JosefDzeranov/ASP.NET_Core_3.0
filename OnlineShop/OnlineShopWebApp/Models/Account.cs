341452c22d6ccc1a814964356a955f9621c69f65
Rasul <rastir@yandex.ru>
2022-04-30 18:16:17 +0300

    Added Login.cshtml, accountController and model account

---
+++ OnlineShop/OnlineShopWebApp/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿
+namespace OnlineShopWebApp.Models
+{
+    public class Account
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
