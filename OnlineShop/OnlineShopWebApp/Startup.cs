f56fff3781bf10e31480d8ea3d60f05fd730ff93
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-05-04 00:50:58 +0300

    Добавил настройки локализации запроса, для решения проблемы валидации decimal

--- OnlineShop/OnlineShopWebApp/Startup.cs
+++ OnlineShop/OnlineShopWebApp/Startup.cs
@@ -1,8 +1,11 @@
+using System.Globalization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using OnlineShopWebApp;
 using OnlineShopWebApp.Interfase;
 
@@ -18,9 +21,20 @@ namespace OnlineShopWebApp
         public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
             services.AddSingleton<IBuyerStorage, JsonBuyerStorage>();
             services.AddSingleton<IProductStorage, JsonProductStorage>();
+            services.AddControllersWithViews();
+
+            services.Configure<RequestLocalizationOptions>(options => //конфигурируем настройки локализации запроса. Решается задача валидации decimal
+            {
+                var supportedCultures = new[]
+                {
+                    new CultureInfo("en-US")
+                };
+                options.DefaultRequestCulture = new RequestCulture("en-US");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -36,6 +50,10 @@ namespace OnlineShopWebApp
             }
             app.UseStaticFiles();
             app.UseRouting();
+
+            //Решается задача валидации decimal
+            var localizationOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>().Value;
+            app.UseRequestLocalization(localizationOptions);
 
             app.UseEndpoints(endpoints =>
             {
