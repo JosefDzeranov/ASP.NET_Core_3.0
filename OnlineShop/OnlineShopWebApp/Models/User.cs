da5dce55296a607a688185799a508776e70189db
mardulius <kotel77@inbox.ru>
2022-05-04 15:44:12 +0300

    adding User class

---
+++ OnlineShop/OnlineShopWebApp/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OnlineShopWebApp.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public bool RememberMe { get; set; }
+        public bool IsAdmin { get; set; }
+
+    }
+}
