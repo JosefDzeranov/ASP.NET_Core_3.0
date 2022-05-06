131cfa73bd842a60f2139a87738517945d4f35ae
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-05-06 10:13:18 +0300

    Добавил поле даты оформления заказа

--- OnlineShop/OnlineShopWebApp/Models/Users/Buyer/OrderItem.cs
+++ OnlineShop/OnlineShopWebApp/Models/Users/Buyer/OrderItem.cs
@@ -1,8 +1,17 @@
-﻿namespace OnlineShopWebApp.Models
+﻿using System;
+
+namespace OnlineShopWebApp.Models
 {
     public class OrderItem
     {
         public CartItem CartItem { get; set; }
         public InfoBuying InfoBuying { get; set; }
+        public DateTime dateTime { get; set; }
+
+        public OrderItem()
+        {
+            dateTime = DateTime.UtcNow;
+        }
+
     }
 }
