239078f1a68917082d5f912a97c71e0715500086
Artem Gilmanov <artemapochta@gmail.com>
2022-05-08 12:00:23 +0200

    Deleted Actions Privacy in HomeController

--- onlineshop/OnlineShopWebApp/Controllers/HomeController.cs
+++ onlineshop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -18,16 +18,5 @@ namespace OnlineShopWebApp.Controllers
             var products = productsRepository.GetAll();
             return View(products);
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
