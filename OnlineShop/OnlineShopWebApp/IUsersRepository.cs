35c92f5a904476f5f9405e7eacbbea334f6b5d36
Татьяна Рыбакова <tatiana_rybakova@mail.ru>
2022-05-08 16:12:40 +0300

    rybakova_lesson7_3

---
+++ OnlineShop/OnlineShopWebApp/IUsersRepository.cs
@@ -0,0 +1,16 @@
+﻿using OnlineShopWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp
+{
+    interface IUsersRepository
+    {
+        List<User> GetAll();
+        User TryGetByName(string name);
+        void Add(User newUser); 
+        void Delete(string name);
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/InMemoryUsersRepository.cs
@@ -0,0 +1,33 @@
+﻿using OnlineShopWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp
+{
+    public class InMemoryUsersRepository : IUsersRepository
+    {
+        List<User> users = new List<User>();
+
+        public List<User> GetAll()
+        {
+            return users;
+        }
+
+        public User TryGetByName(string name)
+        {
+            return users.FirstOrDefault(x => x.Name == name);
+        }
+        public void Add(User newUser)
+        {
+            users.Add(newUser);
+        }
+
+        public void Delete(string name)
+        {
+            users.RemoveAll(x => x.Name == name);
+        }
+
+    }
+}
