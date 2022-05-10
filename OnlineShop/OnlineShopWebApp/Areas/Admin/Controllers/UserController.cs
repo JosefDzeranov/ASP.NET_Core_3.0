188197254937df323f2cc41873e4ca6663d6e66f
Татьяна Рыбакова <tatiana_rybakova@mail.ru>
2022-05-10 12:43:38 +0300

    добавила страницу списка пользователей и детали пользователя

--- OnlineShop/OnlineShopWebApp/Areas/Admin/Controllers/UserController.cs
+++ OnlineShop/OnlineShopWebApp/Areas/Admin/Controllers/UserController.cs
@@ -9,9 +9,30 @@ namespace OnlineShopWebApp.Areas.Admin.Controllers
     [Area("Admin")]
     public class UserController : Controller
     {
+        private readonly IUsersRepository usersRepository;
+
+        public UserController(IUsersRepository usersRepository)
+        {
+            this.usersRepository = usersRepository;
+        }
+
         public IActionResult Users()
         {
-            return View();
+            var users = usersRepository.GetAll();
+            return View(users);
         }
+
+        public IActionResult Details(string userLogin)
+        {
+            var user = usersRepository.TryGetByLogin(userLogin);
+            return View(user);
+        }
+
+        //[HttpPost]
+        //public IActionResult UpdateState(Guid orderId, OrderState state)
+        //{
+        //    ordersRepository.UpdateState(orderId, state);
+        //    return RedirectToAction("Orders");
+        //}
     }
 }
--- OnlineShop/OnlineShopWebApp/Models/UserAccount.cs
+++ OnlineShop/OnlineShopWebApp/Models/UserAccount.cs
@@ -4,5 +4,8 @@
     {
         public string Login { get; set; }
         public string Password { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Email { get; set; }
     }
 }
