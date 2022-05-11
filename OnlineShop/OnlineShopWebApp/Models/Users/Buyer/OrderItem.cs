b76b9fe1630278ac3cdc107a3d94eb75a1e7e063
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-05-11 09:32:35 +0300

    В классе OrderItem добавил поля Status, Login, FullCost, RosterStatus (реестр видов статусов). Добавил методы DataRegistration и TimeRegistration, которые выводят оформленное строковое значение даты и времени. Добавил метод CalculateFullCostOrder, который считает полную стоймость всего заказа Добавил в конструкторе дополнительные действия

--- OnlineShop/OnlineShopWebApp/Models/Users/Buyer/OrderItem.cs
+++ OnlineShop/OnlineShopWebApp/Models/Users/Buyer/OrderItem.cs
@@ -1,16 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OnlineShopWebApp.Models
 {
     public class OrderItem
     {
-        public CartItem CartItem { get; set; }
+        public List<CartItem> CartItem { get; set; }
         public InfoBuying InfoBuying { get; set; }
         public DateTime dateTime { get; set; }
+        public string Status { get; set; }
+        public string Login { get; set; }
+        public decimal FullCost { get; set; }
 
-        public OrderItem()
+        public List<string> RosterStatus { get; set; } = new List<string>()
         {
+            "Создан",
+            "Обработан",
+            "В пути",
+            "Отменён",
+            "Доставлен"
+        };
+
+        public OrderItem(List<CartItem> cartItem, string login, InfoBuying infoBuying)
+        {
+            CartItem = new List<CartItem>(cartItem);
+            CalculateFullCostOrder();
             dateTime = DateTime.UtcNow;
+            Status = RosterStatus[0];
+            Login = login;
+            InfoBuying = infoBuying;
+        }
+
+        public string DataRegistration()
+        {
+            string data = string.Format("{0:00}.{1:00}.{2:0000}", dateTime.Day, dateTime.Month, dateTime.Year);
+            return data;
+        }
+        public string TimeRegistration()
+        { //По Гринвичу
+            string data = string.Format("{0:00}:{1:00}:{2:00}", dateTime.Hour, dateTime.Minute, dateTime.Second);
+            return data;
+        }
+        public void CalculateFullCostOrder()
+        {
+            FullCost = 0;
+            foreach (var cartItem in CartItem)
+            {
+                foreach (var v in cartItem.additionalOptions)
+                {
+                    FullCost += (v.TotalCost() + cartItem.Product.Cost);
+                }
+            }
+
         }
 
     }
