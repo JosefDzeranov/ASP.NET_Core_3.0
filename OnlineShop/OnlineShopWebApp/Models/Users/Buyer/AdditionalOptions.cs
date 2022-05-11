344347be59f9d7b7f4799bec86c4a169addbecea
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-05-11 09:18:51 +0300

    Добавил класс AdditionalOptions (и проверочное значение внутри)

---
+++ OnlineShop/OnlineShopWebApp/Models/Users/Buyer/AdditionalOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineShopWebApp.Models.Users.Buyer
+{
+    public class AdditionalOptions
+    {
+        public decimal opcion1 = 1;
+
+        public decimal TotalCost()
+        {
+            decimal summ = this.opcion1;
+            return summ;
+        }
+    }
+}
