ba54ae0939352555015b887a7df3654a8bc51258
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-05-11 09:21:09 +0300

    В классе CartItem добавил обработку класса AdditionalOptions и поля Count

--- OnlineShop/OnlineShopWebApp/Models/Users/Buyer/CartItem.cs
+++ OnlineShop/OnlineShopWebApp/Models/Users/Buyer/CartItem.cs
@@ -1,11 +1,31 @@
-﻿namespace OnlineShopWebApp.Models
+﻿using System.Collections.Generic;
+using OnlineShopWebApp.Models.Users.Buyer;
+
+namespace OnlineShopWebApp.Models
 {
     public class CartItem
     {
         public Product Product { get; set; }
         public int Count { get; set; }
-        
+        public List<AdditionalOptions> additionalOptions { get; set; } = new List<AdditionalOptions> { };
+
+        public CartItem(Product product)
+        {
+            Product = product;
+            Count = 1;
+            additionalOptions.Add(new AdditionalOptions());
+        }
+        public void UpCount()
+        {
+            Count++;
+            additionalOptions.Add(new AdditionalOptions());
+        }
+
+        public void DownCount()
+        {
+            Count--;
+            additionalOptions.RemoveAt(0);
+        }
+
     }
-
-
 }
