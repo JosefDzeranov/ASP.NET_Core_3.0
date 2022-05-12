afa6fda62cb94cc27af33bfded80089f022cb54a
mardulius <kotel77@inbox.ru>
2022-05-12 10:49:49 +0300

    changing ProductDetails page

--- OnlineShop/OnlineShopWebApp/Services/ProductRepository.cs
+++ OnlineShop/OnlineShopWebApp/Services/ProductRepository.cs
@@ -12,70 +12,120 @@ namespace OnlineShopWebApp.Services
            {
                Name = "Незнайка на луне",
                Cost = 345.6m,
-               Description = "Описание товара",
+               Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" +
+               " eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+               "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi" +
+               " ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit" +
+               " in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur" +
+               " sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                ImgPath ="/images/book.png"
            },
            new Product()
            {
                Name = "Что делать?",
                Cost = 556.5m,
-               Description = "Описание товара",
+               Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" +
+               " eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+               "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi" +
+               " ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit" +
+               " in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur" +
+               " sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                ImgPath ="/images/book.png"
            },
            new Product()
            {
                Name = "Одисея капитана Блада",
                Cost = 1360.4m,
-               Description = "Описание товара",
+               Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" +
+               " eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+               "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi" +
+               " ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit" +
+               " in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur" +
+               " sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                ImgPath ="/images/book.png"
            },
             new Product()
            {
                Name = "Война и мир",
                Cost = 790.6m,
-               Description = "Описание товара",
+               Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" +
+               " eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+               "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi" +
+               " ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit" +
+               " in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur" +
+               " sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                ImgPath ="/images/book.png"
            },
             new Product()
            {
                Name = "Овод",
                Cost = 860.45m,
-               Description = "Описание товара",
+               Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" +
+               " eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+               "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi" +
+               " ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit" +
+               " in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur" +
+               " sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                ImgPath ="/images/book.png"
            },
             new Product()
            {
                Name = "Капитанская дочка",
                Cost = 340.50m,
-               Description = "Описание товара",
+               Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" +
+               " eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+               "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi" +
+               " ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit" +
+               " in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur" +
+               " sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                ImgPath ="/images/book.png"
            },
             new Product()
            {
                Name = "Преступление и наказание",
                Cost = 570.00m,
-               Description = "Описание товара",
+               Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" +
+               " eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+               "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi" +
+               " ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit" +
+               " in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur" +
+               " sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                ImgPath ="/images/book.png"
            },
             new Product()
            {
                Name = "Тихий Дон",
                Cost =1200.00m,
-               Description = "Описание товара",
+               Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" +
+               " eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+               "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi" +
+               " ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit" +
+               " in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur" +
+               " sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                ImgPath ="/images/book.png"
            },
             new Product()
            {
                Name = "Отцы и дети",
                Cost = 459.99m,
-               Description = "Описание товара",
+               Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" +
+               " eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+               "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi" +
+               " ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit" +
+               " in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur" +
+               " sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                ImgPath ="/images/book.png"
            },
             new Product()
            {
                Name = "Богач, бедняк",
                Cost = 1100.85m,
-               Description = "Описание товара",
+               Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" +
+               " eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+               "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi" +
+               " ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit" +
+               " in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur" +
+               " sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                ImgPath ="/images/book.png"
            },
 
