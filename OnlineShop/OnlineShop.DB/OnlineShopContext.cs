f39e17d06b5b7e38b4586f8f8d1d65b4b60443ae
mardulius <kotel77@inbox.ru>
2022-05-14 18:32:31 +0300

    install  EFcore packages and create DBcontext

--- OnlineShop/OnlineShop.DB/OnlineShopContext.cs
+++ OnlineShop/OnlineShop.DB/OnlineShopContext.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace OnlineShop.DB
 {
-    public class OnlineShopContext
+    public class OnlineShopContext : DbContext
     {
+
+        public OnlineShopContext(DbContextOptions<OnlineShopContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
     }
 }
