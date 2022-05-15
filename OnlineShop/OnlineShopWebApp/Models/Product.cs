b963ee36330aa88204aa4075020569aec9decc67
Rasul <rastir@yandex.ru>
2022-05-15 15:11:23 +0300

    Added validation in "add" Product and "Edit" Product.

--- OnlineShop/OnlineShopWebApp/Models/Product.cs
+++ OnlineShop/OnlineShopWebApp/Models/Product.cs
@@ -1,14 +1,24 @@
-﻿namespace OnlineShopWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Не указано наименование услуги")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Не указана стоимость услуги")]
         public decimal Cost { get; set; }
+
+        [Required(ErrorMessage = "Не заполнено описание услуги")]
         public string Description { get; set; }
+
         public string ImagePath { get; set; }
 
         public Product() { }
+
         public Product(int id,string name, decimal cost, string description, string imagePath)
         {
             Id = id;
