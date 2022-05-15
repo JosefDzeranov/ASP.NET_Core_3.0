a219586c0d781b1f721469619bd282d17704bdfd
Rasul <rastir@yandex.ru>
2022-05-15 22:09:44 +0300

    Added use Serilog at file and console

--- OnlineShop/OnlineShopWebApp/Startup.cs
+++ OnlineShop/OnlineShopWebApp/Startup.cs
@@ -2,10 +2,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using OnlineShopWebApp.Interface;
 using OnlineShopWebApp.Models;
 using OnlineShopWebApp.Services;
+using Serilog;
 
 namespace OnlineShopWebApp
 {
@@ -35,6 +35,8 @@ namespace OnlineShopWebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSerilogRequestLogging();
+
             app.UseStaticFiles();
 
             app.UseRouting();
