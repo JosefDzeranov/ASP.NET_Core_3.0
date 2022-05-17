9c45ca6f521143cb28f9276c66abc511d32eaaa1
adjutantee <romashilov3233@gmail.com>
2022-05-17 16:02:22 +0300

    save comit

--- OnlineShop/OnlineShopWebApp/Models/UserDeliveryInfo.cs
+++ OnlineShop/OnlineShopWebApp/Models/UserDeliveryInfo.cs
@@ -1,11 +1,18 @@
-﻿namespace OnlineShopWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
 {
     public class UserDeliveryInfo
     {
+        [Required(ErrorMessage = "Введите имя")]
         public string InputName { get; set; }
+        [Required]
         public string InputLastName { get; set; }
+        [Required(ErrorMessage = "Введите фамилия")]
         public string InputCity { get; set; }
+        [Required(ErrorMessage = "Введите город")]
         public string InputAddress { get; set; }
+        [Required(ErrorMessage = "Введите адрес")]
         public string InputEmail { get; set; }
     }
 }
