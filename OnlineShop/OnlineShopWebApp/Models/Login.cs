ab14b5f6eb6041f6de84d2dafaf4d797e359a83e
adjutantee <romashilov3233@gmail.com>
2022-05-17 14:58:37 +0300

    Validation Accout

--- OnlineShop/OnlineShopWebApp/Models/Login.cs
+++ OnlineShop/OnlineShopWebApp/Models/Login.cs
@@ -1,8 +1,14 @@
-﻿namespace OnlineShopWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
 {
     public class Login
     {
+        [Required(ErrorMessage = "Не указал электронный адрес")]
+        [EmailAddress(ErrorMessage = "Не верный электронный адрес")]
         public string InputEmail1 { get; set; }
+        [Required(ErrorMessage = "Неверно указан пароль")]
+        [StringLength(100, MinimumLength = 4)]
         public string InputPassword { get; set; }
         public bool ExampleCheck1 { get; set; }
     }
