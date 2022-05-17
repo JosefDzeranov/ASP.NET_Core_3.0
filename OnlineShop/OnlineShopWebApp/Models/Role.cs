78f6a30614d63427b5d6771e4d56ed1aee17f347
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-05-17 08:41:12 +0300

    Добавил в класс Role поле Id. Необходимое для того, чтобы была возможность поменять название роли, но привязка пользователей к этой роли не слетела

--- OnlineShop/OnlineShopWebApp/Models/Role.cs
+++ OnlineShop/OnlineShopWebApp/Models/Role.cs
@@ -1,9 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace OnlineShopWebApp.Models
 {
     public class Role
     {
+        public Guid Id { get; set; }
+
         [Required]
         public string Name { get; set; }
     }
