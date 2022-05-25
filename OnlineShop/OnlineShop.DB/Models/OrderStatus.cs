4e12a4a712a139cf34cc5e45712bf5265764c525
mardulius <kotel77@inbox.ru>
2022-05-25 18:03:11 +0300

    adding extention method for display name enum into orderDetails view

--- OnlineShop/OnlineShop.DB/Models/OrderStatus.cs
+++ OnlineShop/OnlineShop.DB/Models/OrderStatus.cs
@@ -1,10 +1,16 @@
-﻿namespace OnlineShop.DB.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.DB.Models
 {
     public enum OrderStatus
     {
+        [Display(Name = "Новый")]
         New,
+        [Display(Name = "Подтвержден")]
         Confirmed,
+        [Display(Name = "Оплачен")]
         Paid,
+        [Display(Name = "Доставлен")]
         Delivered
 
     }
