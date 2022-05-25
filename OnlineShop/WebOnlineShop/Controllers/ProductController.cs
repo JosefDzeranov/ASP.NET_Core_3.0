058bbc9f9ab063a540d8b676bee49dbfbf1fa922
adjutantee <romashilov3233@gmail.com>
2022-05-25 10:21:12 +0300

    save

---
+++ OnlineShop/WebOnlineShop/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebOnlineShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductRepository productRepository;
+
+        public ProductController()
+        {
+            productRepository = new ProductRepository();
+        }
+        public string Index(int id)
+        {
+            var product = productRepository.TryGetById(id);
+        }
+        
+    }
+}
