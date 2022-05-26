006e14455f394846d9202ae20b4b1637293353e7
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-05-26 23:04:59 +0300

    Создал DatabaseContext и Dbset<Product> Добавил класс Product

---
+++ OnlineShop/OnlineShop.Db/DataBaseContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Db.Models;
+
+namespace OnlineShop.Db
+{
+    internal class DatabaseContext:DbContext
+    {
+        //доступ к таблицам
+        public DbSet<Product> Products { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated(); //создаём БД при первом обращении
+        }
+
+    }
+}
---
+++ OnlineShop/OnlineShop.Db/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlineShop.Db.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        public string CodeNumber { get; set; }
+
+        public decimal Cost { get; set; }
+
+        public double Square { get; set; }
+
+        public double Width { get; set; }
+        
+        public double Length { get; set; }
+        
+        public string Description { get; set; }
+        
+        public List<string> Images { get; set; }
+
+        
+    }
+}
