6d2853639baefb9e111d7361a99d1a25e2609180
firokikidreamtek <firokikidreamtek@yandex.ru>
2022-06-06 14:48:37 +0300

    Added IdentityInit Added IdentityContext

---
+++ OnlineShop/OnlineShop.DB/IdentityContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.DB.Models;
+
+namespace OnlineShop.DB
+{
+    public class IdentityContext : IdentityDbContext<User>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+    }
+}
---
+++ OnlineShop/OnlineShop.DB/IdentityInitializer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using OnlineShop.DB.Models;
+
+
+namespace OnlineShop.DB
+{
+    public class IdentityInitializer
+    {
+
+        public static void Initialize(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            var adminEmail = "admin@mail.ru";
+            var adminPassword = "FghTsEwQ74@";
+            if (roleManager.FindByNameAsync(Const.AdminRoleName).Result == null)
+            {
+                roleManager.CreateAsync(new IdentityRole("Admin")).Wait();
+            }
+            if (roleManager.FindByNameAsync(Const.UserRoleName).Result == null)
+            {
+                roleManager.CreateAsync(new IdentityRole(Const.UserRoleName)).Wait();
+            }
+            if (userManager.FindByNameAsync(adminEmail).Result == null)
+            {
+                var admin = new User { Login = adminEmail, UserName = adminEmail };
+                var result = userManager.CreateAsync(admin, adminPassword).Result;
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(admin, Const.AdminRoleName).Wait();
+                }
+            }
+        }
+    }
+}
