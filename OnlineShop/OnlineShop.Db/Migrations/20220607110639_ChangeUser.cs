12b27e967e59b23695b5e34a9352c0b1ef868472
Татьяна Рыбакова <tatiana_rybakova@mail.ru>
2022-06-07 14:14:01 +0300

    исправлено замечание

---
+++ OnlineShop/OnlineShop.Db/Migrations/20220607110639_ChangeUser.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineShop.Db.Migrations.Identity
+{
+    public partial class ChangeUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Login",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "AspNetUsers",
+                newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "AspNetUsers",
+                newName: "Password");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Login",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
