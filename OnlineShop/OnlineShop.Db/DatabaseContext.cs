597ed4ac047a4130d362a53e85323798c8250103
Артем Гильманов <artemapochta@gmail.com>
2022-06-11 19:22:34 +0200

    1. Generated method OnModelCreating in DataBaseContext 2. Aded default products 3. Aded new migration

--- OnlineShop/OnlineShop.Db/DatabaseContext.cs
+++ OnlineShop/OnlineShop.Db/DatabaseContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OnlineShop.Db.Models;
+using System;
+using System.Collections.Generic;
 
 namespace OnlineShop.Db
 {
@@ -14,6 +16,61 @@ namespace OnlineShop.Db
             : base(options)
         {
             Database.Migrate(); // creates data table with the first call
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new List<Product>()
+            {
+                new Product()
+                {
+                    Id=Guid.NewGuid(),
+                    Name="Name1",
+                    Cost=1000,
+                    Description = "Description1",
+                    ImagePath="/images/item1.jpg",
+                },
+                new Product()
+                {
+                    Id=Guid.NewGuid(),
+                    Name="Name2",
+                    Cost=2000,
+                    Description = "Description2",
+                    ImagePath="/images/item2.jpg",
+                },
+                new Product()
+                {
+                    Id=Guid.NewGuid(),
+                    Name="Name3",
+                    Cost=3000,
+                    Description = "Description3",
+                    ImagePath="/images/item3.jpg",
+                },
+                new Product()
+                {
+                    Id=Guid.NewGuid(),
+                    Name="Name4",
+                    Cost=4000,
+                    Description = "Description4",
+                    ImagePath="/images/item4.jpg",
+                },
+                new Product()
+                {
+                    Id=Guid.NewGuid(),
+                    Name="Name5",
+                    Cost=5000,
+                    Description = "Description5",
+                    ImagePath="/images/item5.jpg",
+                },
+                new Product()
+                {
+                    Id= Guid.NewGuid(),
+                    Name="Name6",
+                    Cost=6000,
+                    Description = "Description6",
+                    ImagePath="/images/item6.jpg",
+                },
+
+            }) ;
         }
     }
 }
