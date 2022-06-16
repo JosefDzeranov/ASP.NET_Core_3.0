27e7ec53411b55b857a544f9b3566cb66fcd68e1
mardulius <kotel77@inbox.ru>
2022-06-16 17:19:27 +0300

    fixing  clear Cart errors

---
+++ OnlineShop/OnlineShop.DB/Migrations/20220616141809_FixCartItem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineShop.DB.Migrations
+{
+    public partial class FixCartItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CartId",
+                table: "CartItems",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CartId",
+                table: "CartItems",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- OnlineShop/OnlineShop.DB/Models/Cart.cs
+++ OnlineShop/OnlineShop.DB/Models/Cart.cs
@@ -7,6 +7,6 @@ namespace OnlineShop.DB.Models
     {
         public Guid Id { get; set; }
         public string UserId { get; set; }
-        public virtual List<CartItem> Items { get; set; }
+        public virtual List<CartItem> Items { get; set; } = new List<CartItem>();
     }
 }
