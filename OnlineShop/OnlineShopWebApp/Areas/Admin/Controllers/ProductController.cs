8ae77b99d135e97f080bf9cee248fdcf915d2d08
Татьяна Рыбакова <tatiana_rybakova@mail.ru>
2022-06-16 22:21:35 +0300

    .

--- OnlineShop/OnlineShopWebApp/Areas/Admin/Controllers/ProductController.cs
+++ OnlineShop/OnlineShopWebApp/Areas/Admin/Controllers/ProductController.cs
@@ -1,26 +1,22 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Hosting;
 using OnlineShop.Db;
-using OnlineShop.Db.Models;
 using OnlineShopWebApp.Helpers;
 using OnlineShopWebApp.Models;
 using System;
-using System.IO;
-using System.Linq;
 
 namespace OnlineShopWebApp.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductController : Controller
     {
-        private readonly IProductsRepository productsRepository;
-        private readonly IWebHostEnvironment appEnvironment;
+        private readonly IProductsRepository productsRepository;        
+        private readonly ImagesProvider imagesProvider;
 
-        public ProductController(IProductsRepository productsRepository, IWebHostEnvironment appEnvironment)
+        public ProductController(IProductsRepository productsRepository, ImagesProvider imagesProvider)
         {
-            this.productsRepository = productsRepository;
-            this.appEnvironment = appEnvironment;
+            this.productsRepository = productsRepository;            
+            this.imagesProvider = imagesProvider;
         }
 
         public IActionResult Products()
@@ -35,92 +31,69 @@ namespace OnlineShopWebApp.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public IActionResult Add(ProductViewModel product)
+        public IActionResult Add(AddProductViewModel product)
         {
             if (!ModelState.IsValid)
             {
                 return View(product);
             }
 
-            if (product.UploadedFile != null)
-            {
-                string productImagePath = Path.Combine(appEnvironment.WebRootPath + "/images/products/");
-                if (!Directory.Exists(productImagePath))
-                {
-                    Directory.CreateDirectory(productImagePath);
-                }
-
-                var fileName = Guid.NewGuid() + "." + product.UploadedFile.FileName.Split('.').Last();
-                using (var fileStream = new FileStream(productImagePath + fileName, FileMode.Create))
-                {
-                    product.UploadedFile.CopyTo(fileStream);
-                }
-
-                var productDb = new Product
-                {
-                    Name = product.Name,
-                    Cost = product.Cost,
-                    Description = product.Description,
-                    Pages = product.Pages,
-                    ImagePath = "/images/products/" + fileName
-                };
-
-                productsRepository.Add(productDb);
-            }
+            var imagesPaths = imagesProvider.SaveFiles(product.UploadedFiles, ImagesFolders.products);
+            productsRepository.Add(product.ToProduct(imagesPaths));
             return RedirectToAction("Products");
         }
 
-        public IActionResult Edit(Guid productId)
+        //public IActionResult Edit(Guid productId)
+        //{
+        //    var product = productsRepository.TryGetById(productId);
+        //    return View(new ProductViewModel
+        //    {
+        //        Id = product.Id,
+        //        Name = product.Name,
+        //        Description = product.Description,
+        //        Pages = product.Pages,
+        //        Cost = product.Cost,
+        //        ImagePath = product.ImagePath
+        //    });
+        //}
+        //[HttpPost]
+        //public IActionResult Edit(ProductViewModel changedProduct)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View(changedProduct);
+        //    }
+
+        //    var product = productsRepository.TryGetById(changedProduct.Id);
+
+        //    product.Id = changedProduct.Id;
+        //    product.Name = changedProduct.Name;
+        //    product.Cost = changedProduct.Cost;
+        //    product.Description = changedProduct.Description;
+        //    product.Pages = changedProduct.Pages;
+
+        //    if (changedProduct.UploadedFile != null)
+        //    {
+        //        FileInfo fileInfo = new FileInfo(Path.Combine(appEnvironment.WebRootPath + product.ImagePath));
+        //        fileInfo.Delete();
+        //        string productImagePath = Path.Combine(appEnvironment.WebRootPath + "/images/products/");
+        //        var fileName = Guid.NewGuid() + "." + changedProduct.UploadedFile.FileName.Split('.').Last();
+        //        using (var fileStream = new FileStream(productImagePath + fileName, FileMode.Create))
+        //        {
+        //            changedProduct.UploadedFile.CopyTo(fileStream);
+        //        }
+
+        //        product.ImagePath = "/images/products/" + fileName;                
+        //    }           
+
+        //    productsRepository.Edit();       
+        //    return RedirectToAction("Products");                                   
+        //}
+
+        public IActionResult Delete(Guid productId)
         {
-            var product = productsRepository.TryGetById(productId);
-            return View(new ProductViewModel
-            {
-                Id = product.Id,
-                Name = product.Name,
-                Description = product.Description,
-                Pages = product.Pages,
-                Cost = product.Cost,
-                ImagePath = product.ImagePath
-            });
-        }
-        [HttpPost]
-        public IActionResult Edit(ProductViewModel changedProduct)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(changedProduct);
-            }
-
-            var product = productsRepository.TryGetById(changedProduct.Id);
-
-            product.Id = changedProduct.Id;
-            product.Name = changedProduct.Name;
-            product.Cost = changedProduct.Cost;
-            product.Description = changedProduct.Description;
-            product.Pages = changedProduct.Pages;
-
-            if (changedProduct.UploadedFile != null)
-            {
-                FileInfo fileInfo = new FileInfo(Path.Combine(appEnvironment.WebRootPath + product.ImagePath));
-                fileInfo.Delete();
-                string productImagePath = Path.Combine(appEnvironment.WebRootPath + "/images/products/");
-                var fileName = Guid.NewGuid() + "." + changedProduct.UploadedFile.FileName.Split('.').Last();
-                using (var fileStream = new FileStream(productImagePath + fileName, FileMode.Create))
-                {
-                    changedProduct.UploadedFile.CopyTo(fileStream);
-                }
-
-                product.ImagePath = "/images/products/" + fileName;                
-            }           
-
-            productsRepository.Edit();       
-            return RedirectToAction("Products");                                   
-        }
-
-            public IActionResult Delete(Guid productId)
-            {
-                productsRepository.Delete(productId);
-                return RedirectToAction("Products");
-            }
+            productsRepository.Delete(productId);
+            return RedirectToAction("Products");
         }
     }
+}
