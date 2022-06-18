ead1ffa261b4eec693afb26afc0acb9131fa2cc5
Татьяна Рыбакова <tatiana_rybakova@mail.ru>
2022-06-18 21:16:22 +0300

    rybakova_lesson11_4

---
+++ OnlineShop/OnlineShopWebApp/Views/Shared/Components/Profile/ProfileViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Db.Models;
+using OnlineShopWebApp.Helpers;
+
+namespace OnlineShopWebApp.Views.Shared.Components.Profile
+{
+    public class ProfileViewComponent : ViewComponent
+    {
+        private readonly UserManager<User> userManager;
+
+        public ProfileViewComponent(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var user = userManager.FindByNameAsync(User.Identity.Name).Result;
+            return View(user.ToUserViewModel());
+        }
+    }
+}
