e8cad02dfb354aee6b8ae0312e9e90d7a6e3901d
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-06-27 21:29:24 +0300

    Добавил тестовый контроллер с вьюшкой. +перенёс Employee в модель

---
+++ OnlineShop/OnlineShopWebApp/Controllers/EmployeesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create([Bind("Name,AccountNumber,Age")] Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(employee);
+            }
+
+            _repo.CreateEmployee(employee);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- OnlineShop/OnlineShopWebApp/Models/Employee.cs
+++ OnlineShop/OnlineShopWebApp/Models/Employee.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace OnlineShopWebApp
+namespace OnlineShopWebApp.Models
 {
     [Table("Employee")]
     public class Employee
