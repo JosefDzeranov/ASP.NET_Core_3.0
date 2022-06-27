ff263c882a3cede618041d460ba650faafe8419a
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-06-27 19:11:40 +0300

    Добавил проект для тестирования OnlineShop.AutomatedUITests

---
+++ OnlineShop/OnlineShop.AutomatedUITests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace OnlineShop.AutomatedUITests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
