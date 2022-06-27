5b7013eb6904e48cf9a5fade45058c75ac387c2d
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-06-28 00:25:48 +0300

    Комментарии, почему нужно изолировать зависимости

--- OnlineShop/OnlineShopWebApp/Controllers/EmployeesController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/EmployeesController.cs
@@ -1,16 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OnlineShopWebApp.Interfase;
 using OnlineShopWebApp.Models;
+using OnlineShopWebApp.Validation;
 
 namespace OnlineShopWebApp.Controllers
 {
+    //временный контроллер, для обучения тестированию
     public class EmployeesController : Controller
     {
+        //мы используем Injection Dependency, чтобы внедрить интерфейс в наш контроллер.
+        //Таким образом, наш контроллер зависит от логики репозитория через внедренный интерфейс.
+        //Когда мы пишем тесты для нашего контроллера или любого другого класса в проекте,
+        //мы должны изолировать эти зависимости.
+        //
+        //Изолирование зависимостей в тестовом коде дает несколько преимуществ:
+        //   - Не нужно инициализировать все зависимости, чтобы возвращать правильные значения
+        //   - Если наш тест завершился неудачно и мы не изолировали зависимость, мы не можем быть уверены,
+        //что это не так из-за ошибки в контроллере или в этой зависимости
+        //   - Когда зависимый код взаимодействует с реальной базой данных, как это делает наш репозиторий,
+        //то выполнение тестового кода может занять больше времени из-за проблем с подключением или просто процесса выборки данных.
+
         private readonly IEmployeeRepository _repo;
+        private readonly AccountNumberValidation _validation;
 
         public EmployeesController(IEmployeeRepository employeeRepository)
         {
             _repo = employeeRepository;
+            _validation = new AccountNumberValidation();
         }
         public IActionResult Index()
         {
