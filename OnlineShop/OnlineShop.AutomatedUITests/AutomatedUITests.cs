bbccf007cc1cf5e3a035ab1509e9d3d94d34023c
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-06-27 19:42:15 +0300

    первый тестовый тест - окно браузера Chrom должно открыться и закрыться. И подготовка к следующему тесту

--- OnlineShop/OnlineShop.AutomatedUITests/AutomatedUITests.cs
+++ OnlineShop/OnlineShop.AutomatedUITests/AutomatedUITests.cs
@@ -21,5 +21,17 @@ namespace OnlineShop.AutomatedUITests
             _driver.Quit();
             _driver.Dispose();
         }
+
+        //первый тестовый тест
+        [Fact]
+        public void Create_WhenExecuted_ReturnsCreateView()
+        {
+            _driver.Navigate() // используем метод Navigate, чтобы указать драйверу, что нужно переместить браузер в другое место
+                .GoToUrl("https://localhost:5001/OnlineShop/Create"); //с помощью метода GoToUrl мы указываем это местоположение
+
+            //ѕосле перехода браузера к запрошенному URL-адресу будут заполнены свойства Title и PageSource объекта _driver
+            Assert.Equal("Create - EmployeesApp", _driver.Title); 
+            Assert.Contains("Please provide a new employee data", _driver.PageSource);
+        }
     }
 }
