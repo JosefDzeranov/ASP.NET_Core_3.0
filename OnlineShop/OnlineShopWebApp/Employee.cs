eff9a9e305447f17c7866db15153925814e5bcb1
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-06-27 21:22:08 +0300

    Добавил тестовый класс Employee

---
+++ OnlineShop/OnlineShopWebApp/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineShopWebApp
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Age is required")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Account number is required")]
+        public string AccountNumber { get; set; }
+
+    }
+}
