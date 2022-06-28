f269f056f9ed3efb93b91d092c34dc90bbd10e5c
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-06-28 14:23:13 +0300

    Добавил вьюшки к тестовому контроллеру

--- OnlineShop/OnlineShop.AutomatedUITests/AutomatedUITests.cs
+++ OnlineShop/OnlineShop.AutomatedUITests/AutomatedUITests.cs
@@ -5,53 +5,52 @@ using Xunit;
 
 namespace OnlineShop.AutomatedUITests
 {
-    public class AutomatedUITests//: IDisposable
+    public class AutomatedUITests: IDisposable
     {
         //собираемс€ использовать метод Dispose, чтобы закрыть окно chrome, открытое ChromeDriver, а также удалить его.
 
+        private readonly IWebDriver _driver;
+        public AutomatedUITests()
+        {
+            _driver = new ChromeDriver();
+        }
 
-        //private readonly IWebDriver _driver;  
-        //public AutomatedUITests()
-        //{
-        //    _driver = new ChromeDriver();
-        //}
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
 
-        //public void Dispose()
-        //{
-        //    _driver.Quit();
-        //    _driver.Dispose();
-        //}
+        //первый тест пользовательского интерфейса
+        [Fact]
+        public void Create_WhenExecuted_ReturnsCreateView()
+        {
+            _driver.Navigate() // используем метод Navigate, чтобы указать драйверу, что нужно переместить браузер в другое место
+                .GoToUrl("https://localhost:5001/Employees/Create"); //с помощью метода GoToUrl мы указываем это местоположение
 
-        ////первый тестовый тест
-        //[Fact]
-        //public void Create_WhenExecuted_ReturnsCreateView()
-        //{
-        //    _driver.Navigate() // используем метод Navigate, чтобы указать драйверу, что нужно переместить браузер в другое место
-        //        .GoToUrl("https://localhost:5001/OnlineShop/Index"); //с помощью метода GoToUrl мы указываем это местоположение
+            //ѕосле перехода браузера к запрошенному URL-адресу будут заполнены свойства Title и PageSource объекта _driver
+            Assert.Equal("Create - EmployeesApp", _driver.Title);
+            Assert.Contains("Please provide a new employee data", _driver.PageSource);
+        }
 
-        //    //ѕосле перехода браузера к запрошенному URL-адресу будут заполнены свойства Title и PageSource объекта _driver
-        //    Assert.Equal("Create - EmployeesApp", _driver.Title); 
-        //    Assert.Contains("Please provide a new employee data", _driver.PageSource);
-        //}
+        //второй тест в котором мы провер€ем, по€вл€етс€ ли сообщение об ошибке на экране, если мы заполн€ем некоторые пол€ ввода, а не все из них, и нажимает кнопку "Create":
+        [Fact]
+        public void Create_WrongModelData_ReturnsErrorMessage()
+        {
+            _driver.Navigate()
+                .GoToUrl("https://localhost:5001/Employees/Create");
+            _driver.FindElement(By.Id("Name"))
+                .SendKeys("Test Employee");
 
-        ////второй тест в котором мы провер€ем, по€вл€етс€ ли сообщение об ошибке на экране, если мы заполн€ем некоторые пол€ ввода, а не все из них, и нажимает кнопку "Create":
-        //[Fact]
-        //public void Create_WrongModelData_ReturnsErrorMessage()
-        //{
-        //    _driver.Navigate()
-        //        .GoToUrl("https://localhost:5001/Employees/Create");
-        //    _driver.FindElement(By.Id("Name"))
-        //        .SendKeys("Test Employee");
+            _driver.FindElement(By.Id("Age"))
+                .SendKeys("34");
 
-        //    _driver.FindElement(By.Id("Age"))
-        //        .SendKeys("34");
+            _driver.FindElement(By.Id("Create"))
+                .Click();
 
-        //    _driver.FindElement(By.Id("Create"))
-        //        .Click();
+            var errorMessage = _driver.FindElement(By.Id("AccountNumber-error")).Text;
 
-        //    var errorMessage = _driver.FindElement(By.Id("AccountNumber-error")).Text;
-
-        //    Assert.Equal("Account number is required", errorMessage);
-        //}
+            Assert.Equal("Account number is required", errorMessage);
+        }
     }
 }
