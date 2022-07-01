c7723ec1e306305c1c52c66a4aefc09cd4805b10
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-07-01 11:38:12 +0300

    Создаю класс для тестирования функционала. Наследую IDisposable.

---
+++ OnlineShop/OnlineShop.AutomatedUITests/Login/LoginTests.cs
@@ -0,0 +1,19 @@
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace OnlineShop.AutomatedUITests.Login
+{
+    public class LoginTests: IDisposable
+    {
+        public void Dispose()
+        {
+        }
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
