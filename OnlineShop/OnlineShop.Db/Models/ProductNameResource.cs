dec235ac6793db48df9003f1c782e3cc516ebf04
Anton Kuznetsov <85165903+finjy@users.noreply.github.com>
2022-07-01 16:57:28 +0300

    Change Language property migration

--- OnlineShop/OnlineShop.Db/Models/ProductNameResource.cs
+++ OnlineShop/OnlineShop.Db/Models/ProductNameResource.cs
@@ -9,7 +9,7 @@ namespace OnlineShop.Db.Models
         public int LanguageId { get; set; }
         public string Name { get; set; }
         public string Value { get; set; }
-        public Language Languages { get; set; }
+        public Language Language { get; set; }
         public Product Product { get; set; }
     }
 }
