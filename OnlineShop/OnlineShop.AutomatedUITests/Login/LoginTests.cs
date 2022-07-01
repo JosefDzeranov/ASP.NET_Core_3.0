2912c9251a863f98a1ecd849f8b6cdb11876bccb
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-07-01 11:38:55 +0300

    Создаю экземпляр объекта IWebDriver, используя класс ChromeDriver

--- OnlineShop/OnlineShop.AutomatedUITests/Login/LoginTests.cs
+++ OnlineShop/OnlineShop.AutomatedUITests/Login/LoginTests.cs
@@ -7,9 +7,16 @@ namespace OnlineShop.AutomatedUITests.Login
 {
     public class LoginTests: IDisposable
     {
+        private readonly IWebDriver _driver;
+
+        public LoginTests()
+        {
+            _driver = new ChromeDriver();
+        }
         public void Dispose()
         {
         }
+
         [Fact]
         public void Test1()
         {
